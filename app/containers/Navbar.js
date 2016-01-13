21f3b6decda61fc14d7086336e5ca8c9fc2c3f0a
Guy Lepage <guy@onename.com>
2016-01-13 15:25:32 -0500

    updating header so that we can utilize bootstrap full wider searh bar

--- app/containers/Navbar.js
+++ app/containers/Navbar.js
@@ -9,29 +9,33 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-nav navbar-fixed-top navbar-light bg-faded">
-        <div className="nav-item nav-link">
-          <BackButton>
-            <img src="images/icon-previous.svg"/>
-          </BackButton>
+        <div className="nav-item">
+          <a className="nav-link">
+            <BackButton>
+              <img src="images/icon-previous.svg"/>
+            </BackButton>
+          </a>
         </div>
-        <div className="nav-item nav-link">
-          <ForwardButton>
-            <img src="images/icon-next.svg"/>
-          </ForwardButton>
+        <div className="nav-item">
+          <a className="nav-link">
+            <ForwardButton>
+              <img src="images/icon-next.svg"/>
+            </ForwardButton>
+          </a>
         </div>
         <div className="nav-item">
           <form className="form-inline">
             <input className="form-control form-control-sm" type="text" placeholder="search" />
           </form>
         </div>
-        <div className="nav-item nav-link pull-xs-right">
-          <Link to="/settings">
+        <div className="nav-item pull-xs-right">
+          <Link to="/settings" className="nav-link">
             <img src="images/ui-32px-outline-1_settings-gear-64.svg"
               className="nav-icon"/>
           </Link>
         </div>
         <div className="nav-item nav-link pull-xs-right">
-          <Link to="/notifications">
+          <Link to="/notifications" className="nav-link">
             <img src="images/design-32px-outline_bullet-list-67.svg"
               className="nav-icon"/>
           </Link>
