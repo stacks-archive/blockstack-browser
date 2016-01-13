6ca7461b203ef3b2f780293e6bb74d9d522376f5
Guy Lepage <guy@onename.com>
2016-01-13 18:17:51 -0500

    resolved search bar in header full width

--- app/containers/Navbar.js
+++ app/containers/Navbar.js
@@ -8,37 +8,35 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-nav navbar-fixed-top navbar-light bg-faded">
-        <div className="nav-item">
-          <a className="nav-link">
+      <nav className="navbar navbar-light bg-faded navbar-fixed-top">
+        <div className="nav navbar-nav">
+          <div className="nav-item nav-link">
             <BackButton>
               <img src="images/icon-previous.svg"/>
             </BackButton>
-          </a>
-        </div>
-        <div className="nav-item">
-          <a className="nav-link">
+          </div>
+          <div className="nav-item nav-link">
             <ForwardButton>
               <img src="images/icon-next.svg"/>
             </ForwardButton>
-          </a>
-        </div>
-        <div className="nav-item">
-          <form className="form-inline">
-            <input className="form-control form-control-sm" type="text" placeholder="search" />
-          </form>
-        </div>
-        <div className="nav-item pull-xs-right">
-          <Link to="/settings" className="nav-link">
-            <img src="images/ui-32px-outline-1_settings-gear-64.svg"
-              className="nav-icon"/>
-          </Link>
-        </div>
-        <div className="nav-item nav-link pull-xs-right">
-          <Link to="/notifications" className="nav-link">
-            <img src="images/design-32px-outline_bullet-list-67.svg"
-              className="nav-icon"/>
-          </Link>
+          </div>
+          <div className="nav-link nav-search">
+            <form className="">
+              <input className="form-control form-control-sm" type="text" placeholder="search" />
+            </form>
+          </div>
+          <div className="nav-item nav-link pull-xs-right">
+            <Link to="/settings" className="nav-link">
+              <img src="images/ui-32px-outline-1_settings-gear-64.svg"
+                className="nav-icon"/>
+            </Link>
+          </div>
+          <div className="nav-item nav-link pull-xs-right">
+            <Link to="/notifications" className="nav-link">
+              <img src="images/design-32px-outline_bullet-list-67.svg"
+                className="nav-icon"/>
+            </Link>
+          </div>
         </div>
       </nav>
     )
