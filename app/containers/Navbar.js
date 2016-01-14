7759a606a9db21360dd7ea130afe2ebae6b9631c
Guy Lepage <guy@onename.com>
2016-01-13 19:22:24 -0500

    resolved 3 div header sections to inline alignment vertically

--- app/containers/Navbar.js
+++ app/containers/Navbar.js
@@ -10,32 +10,39 @@ export default class Navbar extends Component {
     return (
       <nav className="navbar navbar-light bg-faded navbar-fixed-top">
         <div className="nav navbar-nav">
-          <div className="nav-item nav-link">
-            <BackButton>
-              <img src="images/icon-previous.svg"/>
-            </BackButton>
+          <div className="pull-xs-left">
+            <div className="nav-item nav-link">
+              <BackButton>
+                <img src="images/icon-previous.svg"/>
+              </BackButton>
+            </div>
+            <div className="nav-item nav-link">
+              <ForwardButton>
+                <img src="images/icon-next.svg"/>
+              </ForwardButton>
+            </div>
           </div>
-          <div className="nav-item nav-link">
-            <ForwardButton>
-              <img src="images/icon-next.svg"/>
-            </ForwardButton>
+          
+          <div className="pull-xs-right">
+            <div className="nav-item nav-link">
+              <Link to="/settings">
+                <img src="images/ui-32px-outline-1_settings-gear-64.svg"
+                  className="nav-icon"/>
+              </Link>
+            </div>
+            <div className="nav-item nav-link">
+              <Link to="/notifications">
+                <img src="images/design-32px-outline_bullet-list-67.svg"
+                  className="nav-icon"/>
+              </Link>
+            </div>
           </div>
-          <div className="nav-link nav-search">
-            <form className="">
-              <input className="form-control form-control-sm" type="text" placeholder="search" />
-            </form>
-          </div>
-          <div className="nav-item nav-link pull-xs-right">
-            <Link to="/settings" className="nav-link">
-              <img src="images/ui-32px-outline-1_settings-gear-64.svg"
-                className="nav-icon"/>
-            </Link>
-          </div>
-          <div className="nav-item nav-link pull-xs-right">
-            <Link to="/notifications" className="nav-link">
-              <img src="images/design-32px-outline_bullet-list-67.svg"
-                className="nav-icon"/>
-            </Link>
+          <div className="nav-search ">
+            <div className="nav-link">
+              <form className="form-group">
+                <input className="form-control form-control-sm" type="text" placeholder="search" />
+              </form>
+            </div>
           </div>
         </div>
       </nav>
