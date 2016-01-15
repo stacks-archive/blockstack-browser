a4da8ee4fb90e6bfff2401cd1685ceef35a6a76f
Ryan Shea <ryan@shea.io>
2016-01-15 18:20:28 -0500

    update the settings page so the user can update the API and reset it; update the settings data store; closes #7

--- app/actions/settings.js
+++ app/actions/settings.js
@@ -9,3 +9,15 @@ export function updateApi(nameLookupUrl, searchUrl, registerUrl, addressLookupUr
     addressLookupUrl: addressLookupUrl
   }
 }
+
+export function resetApi() {
+  return dispatch => {
+    const DEFAULT_API = {
+      nameLookupUrl: 'https://api.onename.com/v1/users/{name}',
+      searchUrl: 'https://api.onename.com/v1/search?query={query}',
+      registerUrl: 'https://api.onename.com/v1/users',
+      addressLookupUrl: 'https://api.onename.com/v1/addresses/{address}/names?app-id=73146f6a06443a3a66a7df9473353cde&app-secret=7009a810943a00fe7e8157f27bf91bea7e1b4d4e46db695ba2d11e4333ea6f29'
+    }
+    dispatch(updateApi(DEFAULT_API.nameLookupUrl, DEFAULT_API.searchUrl, DEFAULT_API.registerUrl, DEFAULT_API.addressLookupUrl))
+  }
+}
