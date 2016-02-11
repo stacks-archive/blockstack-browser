481c33cdf4e64c513bb4119da22ac035912f0f60
Guy Lepage <guy@onename.com>
2016-02-10 22:18:28 -0500

    update to export page

--- app/pages/names/ExportPage.js
+++ app/pages/names/ExportPage.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 
+import { PageHeader } from '../../components/index'
 import { InputGroup } from '../../components/index'
 
 class ExportPage extends Component {
@@ -9,13 +10,16 @@ class ExportPage extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div>
-          <h3>Export Identity</h3>
-          <p>Export your identity to another address.</p>
-          <InputGroup label="Recipient address" placeholder="Recipient address" />
+      <div className="body-inner body-inner-white">
+        <PageHeader title="Export" />
+        <div className="container">
           <div>
-            <button className="btn btn-primary">Send</button>
+            <h3>Export Identity</h3>
+            <p>Export your identity to another address.</p>
+            <InputGroup label="Recipient address" placeholder="Recipient address" />
+            <div>
+              <button className="btn btn-primary">Send</button>
+            </div>
           </div>
         </div>
       </div>
