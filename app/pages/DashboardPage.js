3de4f60ab1db59f2f63fe4158d1a7836634635dd
guy <guy@blockstack.com>
2016-08-12 09:13:08 -0400

    added full responsive image technique to app banners

--- app/pages/DashboardPage.js
+++ app/pages/DashboardPage.js
@@ -23,20 +23,20 @@ class DashboardPage extends Component {
   render() {
     return (
       <div className="">
-        <section className="container-fluid wrapper">
+        <section className="">
           <div className="container-fluid">
             <h6 className="clean-font text-xs-center m-t-1 m-b-1">Featured Apps (coming soon...)</h6> 
           </div>
-          <div className="app-container">
-            <div className="app-box-container app-box-gutter">
+          <div className="col-sm-12 app-container no-padding">
+            <div className="col-sm-4 app-box-container app-box-gutter no-padding">
               <img className="app-box" src="images/app-box-chord.jpg" />
               <img className="app-icon" src="images/icon-chord.png" />
             </div>
-            <div className="app-box-container app-box-gutter">
+            <div className="col-sm-4 app-box-container app-box-gutter no-padding">
               <img className="app-box" src="images/app-box-greenleaf.jpg" />
               <img className="app-icon" src="images/icon-greenleaf.png" />
             </div>
-            <div className="app-box-container">
+            <div className="col-sm-4 app-box-container no-padding">
               <img className="app-box" src="images/app-box-guild.jpg" />
               <img className="app-icon" src="images/icon-guild.png" />
             </div>
