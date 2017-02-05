f685282dcba56d6dfc24882b84e26371e71281e6
Guy Lepage <guylepage3@gmail.com>
2017-02-04 20:27:25 -0500

    navbar update from b4 to b3

--- app/js/pages/IdentityPage.js
+++ app/js/pages/IdentityPage.js
@@ -61,10 +61,10 @@ class IdentityPage extends Component {
           <a className="navbar-brand" href="#">
             <img src="/images/app-icon-profiles.png" />
           </a>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          <div className="navbar-collapse" id="navbarSupportedContent">
+            <ul className="nav navbar-nav">
+              <li className="nav-item">
+                Profiles
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">Link</a>
