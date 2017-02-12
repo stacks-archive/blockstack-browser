34aa11b0efc8d25c6a0b6665dc49617c25041758
Larry Salibra <larry@appartisan.com>
2017-02-12 21:45:39 +0200

    remove mock api server from build process & mac app

--- gulp/tasks/compileBlockstackMockCoreForMac.js
+++
@@ -1,7 +0,0 @@
-import gulp from 'gulp'
-var shell = require('gulp-shell')
-
-
-gulp.task('compileBlockstackMockCoreForMac', shell.task([
-  'nexe -i ./utils/mockBlockstackCoreApi.js -o ./native/macos/Blockstack/mockBlockstackCoreApi'
-]))
