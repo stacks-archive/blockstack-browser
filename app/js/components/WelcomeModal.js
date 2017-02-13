cf89208f5f2ac2b4b089c249240376263252638c
Guy Lepage <guylepage3@gmail.com>
2017-02-13 10:15:21 -0500

    update to modal

--- app/js/components/WelcomeModal.js
+++ app/js/components/WelcomeModal.js
@@ -73,23 +73,21 @@ class WelcomeModal extends Component {
     }
 
     return (
-      <div>
+      <div className="">
         <Modal
           isOpen={isOpen}
           onRequestClose={this.props.closeModal}
           contentLabel="This is My Modal"
           shouldCloseOnOverlayClick={false}
           style={{overlay: {zIndex: 10}}}
+          className="container-fluid"
         >
           <h2>Welcome to Blockstack</h2>
-
           { page === 1 ?
             <div>
               <h4>Step 1: Create an account</h4>
-
               <InputGroup name="password" label="Password" type="password"
                 data={this.state} onChange={this.onValueChange} />
-
               <div className="container m-t-40">
                 <button className="btn btn-primary" onClick={this.createAccount}>
                   Create Account
@@ -97,11 +95,9 @@ class WelcomeModal extends Component {
               </div>
             </div>
           : null }
-
           { page === 2 ?
             <div>
               <h4>Step 2: Setup your storage</h4>
-
               <div className="container m-t-40">
                 <button onClick={this.connectDropbox}
                   className="btn btn-sm btn-outline-primary">
