9ecf593ec75ccdbfb1e6e480c0f4955cb6b2d15b
Guy Lepage <guylepage3@gmail.com>
2017-02-18 20:15:07 -0500

    new dashboard ui build

--- app/js/pages/DashboardPage.js
+++ app/js/pages/DashboardPage.js
@@ -19,47 +19,42 @@ class DashboardPage extends Component {
 
   render() {
     return (
-      <div className="container-fluid dashboard">
-        <div className="container profile-wrap-wide">
-          <section>
-            <div className="container-fluid no-padding">
-              <div className="col-sm-12 app-container no-padding">
-                <div className="col-sm-4">
-                  <Link to="/profiles"
-                        className="app-box-container" target="_blank">
-                    <div className="app-box container-fluid">
-                      <img src="/images/app-icon-profiles@2x.png" />
-                    </div>
-                  </Link>
-                  <div className="app-text-container">
-                    <h3>Profiles</h3>
-                  </div>
+      <div className="container-fluid app-center dashboard">
+        <div className="container app-wrap">
+          <div className="col-sm-12 app-container no-padding">
+            <div className="col-sm-4">
+              <Link to="/profiles" className="app-box-container" target="_blank">
+                <div className="app-box container-fluid">
+                  <img src="/images/app-icon-profiles@2x.png" />
                 </div>
-                <div className="col-sm-4">
-                  <Link to="/account/settings"
-                        className="app-box-container" target="_blank">
-                    <div className="app-box">
-                      <img src="/images/app-settings.png" />
-                    </div>
-                  </Link>
-                  <div className="app-text-container">
-                    <h3>Settings</h3>
-                  </div>
-                </div>
-                <div className="col-sm-4">
-                  <a href="https://helloblockstack.com"
-                     className="app-box-container" target="_blank">
-                    <div className="app-box">
-                      <img src="/images/app-hello-blockstack.png" />
-                    </div>
-                  </a>
-                  <div className="app-text-container">
-                    <h3>Hello, Blockstack</h3>
-                  </div>
-                </div>
+              </Link>
+              <div className="app-text-container">
+                <h3>Profiles</h3>
               </div>
             </div>
-          </section>
+            <div className="col-sm-4">
+              <Link to="/account/settings"
+                    className="app-box-container" target="_blank">
+                <div className="app-box">
+                  <img src="/images/app-settings.png" />
+                </div>
+              </Link>
+              <div className="app-text-container">
+                <h3>Settings</h3>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <a href="https://helloblockstack.com"
+                 className="app-box-container" target="_blank">
+                <div className="app-box">
+                  <img src="/images/app-hello-blockstack.png" />
+                </div>
+              </a>
+              <div className="app-text-container">
+                <h3>Hello, Blockstack</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
