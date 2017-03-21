8271252c0d21ce56ba2033c6dadf9f830066f63f
Larry Salibra <larry@appartisan.com>
2017-03-22 04:11:09 +0800

    update dev mode gulp tasks to replace placeholder api password #278

--- gulp/tasks/development.js
+++ gulp/tasks/development.js
@@ -4,18 +4,7 @@ import gulp        from 'gulp';
 import runSequence from 'run-sequence';
 
 gulp.task('dev', ['clean'], function(cb) {
-  console.log(process.argv)
-  if(process.argv.length < 5) {
-    console.error("Please provide your Core API password as an argument to this npm script:")
-    console.error("npm run dev -- --api-password <password>")
-    console.error("or")
-    console.error("npm run dev-ui -- --api-password <password>")
-    process.exit()
-  } else {
-    var spawn = require('child_process').spawn;
-    console.log("Configuring Core API password...")
-    require('fs').writeFileSync('./app/js/utils/core-api-password.js', `export const CORE_API_PASSWORD = '${process.argv[4]}'`);
-  }
+  
   cb = cb || function() {};
 
   global.isProd = false;
