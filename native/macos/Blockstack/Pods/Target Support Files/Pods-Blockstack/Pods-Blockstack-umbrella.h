16ab201ba158012d9e3783e0a54f42586fe08a55
Larry Salibra <larry@appartisan.com>
2017-03-26 16:24:09 +0800

    add CocoaPods #355

---
+++ native/macos/Blockstack/Pods/Target Support Files/Pods-Blockstack/Pods-Blockstack-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <Cocoa/Cocoa.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_BlockstackVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BlockstackVersionString[];
+
---
+++ native/macos/Blockstack/Pods/Target Support Files/Pods-BlockstackTests/Pods-BlockstackTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlockstackTests : NSObject
+@end
+@implementation PodsDummy_Pods_BlockstackTests
+@end
