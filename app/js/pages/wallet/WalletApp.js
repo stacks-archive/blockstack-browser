4c78602576b8e054a5898fc3626a7016d9a8c7b1
Guy Lepage <guylepage3@gmail.com>
2017-03-31 17:09:36 -0400

    added new app descriptor section to wallet app

--- app/js/pages/wallet/WalletApp.js
+++ app/js/pages/wallet/WalletApp.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { Link } from 'react-router'
 
 import { WalletSidebar, PageHeader, StatusBar } from '../../components/index'
 
@@ -30,14 +31,29 @@ class WalletApp extends Component {
     return (
       <div className="body-inner bkg-green">
           <StatusBar />
-        <div className="home-wallet">
-        </div>
-        <div className="container wallet-container">
-          <div className="col-md-4 col-lg-3 wallet-sidebar">
-            <WalletSidebar activeTab={activeTabUrl} />
-          </div>
-          <div className="col-md-8 col-lg-9 wallet-content">
-            {this.props.children}
+        <div className="site-wrapper">
+          <nav className="navbar navbar-toggleable-md navbar-light">
+            <Link to="/wallet/receive" className="navbar-brand">
+              <img src="../images/app-icon-wallet-card-flat.png" />
+            </Link>
+            <div className="navbar-collapse" id="navbarSupportedContent">
+              <ul className="nav navbar-nav m-b-20">
+                <li className="navbar-text">
+                  Profiles
+                </li>
+                <li className="navbar-text navbar-text-secondary">
+                  Utility
+                </li>
+              </ul>
+            </div>
+          </nav>  
+          <div className="container wallet-container">
+            <div className="col-md-4 col-lg-3 wallet-sidebar">
+              <WalletSidebar activeTab={activeTabUrl} />
+            </div>
+            <div className="col-md-8 col-lg-9 wallet-content">
+              {this.props.children}
+            </div>
           </div>
         </div>
       </div>
