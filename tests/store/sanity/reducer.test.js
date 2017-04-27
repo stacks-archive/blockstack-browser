3552c241641e6b25c30444068e7a3c18624b199a
Larry Salibra <larry@appartisan.com>
2017-04-27 23:40:59 +0800

    add verify core api password reducer tests #417

--- tests/store/sanity/reducer.test.js
+++ tests/store/sanity/reducer.test.js
@@ -5,7 +5,8 @@ import {
 describe('Sanity Store: SanityReducer', () => {
   it('should return the proper initial state', () => {
     const initialState = {
-      coreApiRunning: false
+      coreApiRunning: false,
+      coreApiPasswordValid: false
     }
     assert.deepEqual(
       SanityReducer(undefined, {}),
@@ -14,11 +15,13 @@ describe('Sanity Store: SanityReducer', () => {
 
   it('should indicate core is not running', () => {
     const initialState = {
-      coreApiRunning: true
+      coreApiRunning: true,
+      coreApiPasswordValid: false
     }
     const action = SanityActions.coreIsNotRunning()
     const expectedState = {
-      coreApiRunning: false
+      coreApiRunning: false,
+      coreApiPasswordValid: false
     }
     const actualState = SanityReducer(initialState, action)
     assert.deepEqual(actualState, expectedState)
@@ -26,13 +29,47 @@ describe('Sanity Store: SanityReducer', () => {
 
   it('should indicate core is running', () => {
     const initialState = {
-      coreApiRunning: false
+      coreApiRunning: false,
+      coreApiPasswordValid: false
     }
     const action = SanityActions.coreIsRunning()
     const expectedState = {
-      coreApiRunning: true
+      coreApiRunning: true,
+      coreApiPasswordValid: false
     }
     const actualState = SanityReducer(initialState, action)
+    assert.deepEqual(actualState, expectedState)
+  })
+
+  it('should indicate core api password is valid & core is running', () => {
+    const initialState = {
+      coreApiRunning: false,
+      coreApiPasswordValid: false
+    }
+    const expectedState = {
+      coreApiRunning: true,
+      coreApiPasswordValid: true
+    }
+    let action = SanityActions.coreIsRunning()
+    let actualState = SanityReducer(initialState, action)
+    action = SanityActions.coreApiPasswordIsValid()
+    actualState = SanityReducer(actualState, action)
+    assert.deepEqual(actualState, expectedState)
+  })
+
+  it('should indicate core api password is invalid & core is running', () => {
+    const initialState = {
+      coreApiRunning: false,
+      coreApiPasswordValid: false
+    }
+    const expectedState = {
+      coreApiRunning: true,
+      coreApiPasswordValid: false
+    }
+    let action = SanityActions.coreIsRunning()
+    let actualState = SanityReducer(initialState, action)
+    action = SanityActions.coreApiPasswordIsNotValid()
+    actualState = SanityReducer(actualState, action)
     assert.deepEqual(actualState, expectedState)
   })
 })
