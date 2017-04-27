87f87dd656c1e05a1bed995b75cfdb142a1ef431
Larry Salibra <larry@appartisan.com>
2017-04-27 23:52:31 +0800

    add verify core api password sync actions tests #417

--- tests/store/sanity/actions/sync.test.js
+++ tests/store/sanity/actions/sync.test.js
@@ -1,6 +1,26 @@
 import { SanityActions } from '../../../../app/js/store/sanity'
 
 describe('Sanity Store: Sync Actions', () => {
+  describe('coreApiPasswordIsNotValid', () => {
+    it('should return an action of type CORE_API_PASSWORD_NOT_VALID', () => {
+      const expectedResult = {
+        type: 'CORE_API_PASSWORD_NOT_VALID'
+      }
+      const actualResult = SanityActions.coreApiPasswordIsNotValid()
+      assert.deepEqual(actualResult, expectedResult)
+    })
+  })
+
+  describe('coreApiPasswordIsValid', () => {
+    it('should return an action of type CORE_API_PASSWORD_VALID', () => {
+      const expectedResult = {
+        type: 'CORE_API_PASSWORD_VALID'
+      }
+      const actualResult = SanityActions.coreApiPasswordIsValid()
+      assert.deepEqual(actualResult, expectedResult)
+    })
+  })
+
   describe('coreIsNotRunning', () => {
     it('should return an action of type CORE_IS_NOT_RUNNING', () => {
       const expectedResult = {
