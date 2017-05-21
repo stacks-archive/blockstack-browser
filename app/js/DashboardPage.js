0025d0c78559651189c989af47e1041e3e4b79af
Guy Lepage <guylepage3@gmail.com>
2017-05-20 22:32:39 -0400

    cleaned up html

--- app/js/DashboardPage.js
+++ app/js/DashboardPage.js
@@ -22,66 +22,66 @@ class DashboardPage extends Component {
   render() {
     return (
     <div className="home-screen-wrap">
-    <StatusBar />
-    <div className="home-screen">
-      <div className="container-fluid app-center">
-        <div className="container app-wrap">          
-          <div className="app-container no-padding">
-            <div className="app-box-wrap">
-              <Link to="/profiles" className="app-box-container">
-                <div className="app-box">
-                  <img src="/images/app-icon-profiles@2x.png" />
+      <StatusBar />
+      <div className="home-screen">
+        <div className="container-fluid app-center">
+          <div className="container app-wrap">          
+            <div className="app-container no-padding">
+              <div className="app-box-wrap">
+                <Link to="/profiles" className="app-box-container">
+                  <div className="app-box">
+                    <img src="/images/app-icon-profiles@2x.png" />
+                  </div>
+                </Link>
+                <div className="app-text-container">
+                  <h3>Profiles</h3>
                 </div>
-              </Link>
-              <div className="app-text-container">
-                <h3>Profiles</h3>
               </div>
-            </div>
-            <div className="app-box-wrap">
-              <Link to="/storage/providers" className="app-box-container">
-                <div className="app-box">
-                  <img src="/images/app-icon-storage-light@2x.png" />
+              <div className="app-box-wrap">
+                <Link to="/storage/providers" className="app-box-container">
+                  <div className="app-box">
+                    <img src="/images/app-icon-storage-light@2x.png" />
+                  </div>
+                </Link>
+                <div className="app-text-container">
+                  <h3>Storage</h3>
                 </div>
-              </Link>
-              <div className="app-text-container">
-                <h3>Storage</h3>
               </div>
-            </div>
-            <div className="app-box-wrap">
-              <Link to="/wallet/receive" className="app-box-container">
-                <div className="app-box">
-                  <img src="/images/app-icon-wallet-card-flat@2x.png" />
+              <div className="app-box-wrap">
+                <Link to="/wallet/receive" className="app-box-container">
+                  <div className="app-box">
+                    <img src="/images/app-icon-wallet-card-flat@2x.png" />
+                  </div>
+                </Link>
+                <div className="app-text-container">
+                  <h3>Wallet</h3>
                 </div>
-              </Link>
-              <div className="app-text-container">
-                <h3>Wallet</h3>
               </div>
-            </div>
-            <div className="app-box-wrap">
-              <Link to="/account/password" className="app-box-container">
-                <div className="app-box">
-                  <img src="/images/app-icon-settings@2x.png" />
+              <div className="app-box-wrap">
+                <Link to="/account/password" className="app-box-container">
+                  <div className="app-box">
+                    <img src="/images/app-icon-settings@2x.png" />
+                  </div>
+                </Link>
+                <div className="app-text-container">
+                  <h3>Account</h3>
                 </div>
-              </Link>
-              <div className="app-text-container">
-                <h3>Account</h3>
               </div>
-            </div>
-            <div className="app-box-wrap">
-              <a href="https://helloblockstack.com"
-                 className="app-box-container">
-                <div className="app-box">
-                  <img src="/images/app-icon-hello-blockstack@2x.png" />
+              <div className="app-box-wrap">
+                <a href="https://helloblockstack.com"
+                   className="app-box-container">
+                  <div className="app-box">
+                    <img src="/images/app-icon-hello-blockstack@2x.png" />
+                  </div>
+                </a>
+                <div className="app-text-container">
+                  <h3>Hello, Blockstack</h3>
                 </div>
-              </a>
-              <div className="app-text-container">
-                <h3>Hello, Blockstack</h3>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
     )
   }
