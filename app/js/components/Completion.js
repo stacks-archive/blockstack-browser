2eaf067ab5e720765b166e0c70626d0050d08229
Guy Lepage <guylepage3@gmail.com>
2017-05-22 11:30:54 -0400

    resolved compile error in completion.js

--- app/js/components/Completion.js
+++ app/js/components/Completion.js
@@ -3,14 +3,11 @@ import { Link } from 'react-router'
 
 class Completion extends Component {
   static propTypes = {
-    $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
-    })
   }
 
   render() {
     return (
-      <div class="tooltip tooltip-bottom" role="tooltip">
+      <div class="tooltip tooltip-bottom " role="tooltip">
         <div class="tooltip-inner">
           Tooltip on the bottom
         </div>
