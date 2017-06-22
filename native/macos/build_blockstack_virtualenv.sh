d363757e4e4e9dd352e5261ae428e2451844f61d
Larry Salibra <larry@appartisan.com>
2017-06-23 05:13:05 +0800

    update core with session token fixes

--- native/macos/build_blockstack_virtualenv.sh
+++ native/macos/build_blockstack_virtualenv.sh
@@ -48,7 +48,7 @@ pip install --upgrade git+https://github.com/blockstack/zone-file-py.git@7373961
 
 echo "Installing latest blockstack..."
 
-pip install --upgrade git+https://github.com/blockstack/blockstack-core.git@412a9d2090b955bade2a7bd6c8dba035d02135c4
+pip install --upgrade git+https://github.com/blockstack/blockstack-core.git@a47550905a2ae22cd388416220a46b5381c69210
 
 echo "Blockstack virtual environment created."
 
