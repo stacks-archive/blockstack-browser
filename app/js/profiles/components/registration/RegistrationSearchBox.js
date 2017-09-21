83a5ec294176e5089d3867b9fd6345247ff35a11
Alex Lee <alex@uniteus.com>
2017-09-21 18:33:25 -0400

    Fix Search button styles and refactor button vs. link logic

--- app/js/profiles/components/registration/RegistrationSearchBox.js
+++ app/js/profiles/components/registration/RegistrationSearchBox.js
@@ -1,50 +1,74 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 import Alert from '../../../components/Alert'
+import { browserHistory } from 'react-router'
 
-const RegistrationSearchBox = (props) =>
-  (
-  <div>
-    <h3 className="modal-heading">Search for your username</h3>
-    <div className="modal-body">
-    {
-      props.alerts.map((alert, index) =>
-         (
-        <Alert
-          key={index} message={alert.message} status={alert.status} url={alert.url}
-        />
-         )
-      )
-    }
-      <p>
-        Add a username to save your profile so you can interact with other
-        people on the decentralized internet.
-      </p>
-      <form
-        className="container-fluid"
-        onSubmit={props.search}
-        style={{ width: '90%' }}
-      >
-        <input
-          name="username"
-          className="form-control text-xs-center"
-          placeholder="Username"
-          value={props.username}
-          onChange={props.onChange}
-          required
-          disabled={props.disabled}
-          style={{ marginBottom: '5px' }}
-        />
-        <button
-          type="submit"
-          className="btn btn-primary btn-block"
-          disabled={props.disabled}
-        >
-          Search
-        </button>
-      </form>
-    </div>
-  </div>
- )
+class RegistrationSearchBox extends Component {
+  static propTypes = {
+    alerts: PropTypes.array.isRequired,
+    search: PropTypes.func.isRequired,
+    username: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    disabled: PropTypes.bool.isRequired
+  }
+
+  onCancelClick() {
+    browserHistory.push('/profiles')
+  }
+
+  render() {
+    return (
+      <div>
+        <h3 className="modal-heading">Search for your username</h3>
+        <div className="modal-body">
+        {
+          this.props.alerts.map((alert, index) =>
+             (
+            <Alert
+              key={index} message={alert.message} status={alert.status} url={alert.url}
+            />
+             )
+          )
+        }
+          <p>
+            Add a username to save your profile so you can interact with other
+            people on the decentralized internet.
+          </p>
+          <form
+            className="container-fluid"
+            onSubmit={this.props.search}
+            style={{ width: '90%' }}
+          >
+            <input
+              name="username"
+              className="form-control text-xs-center"
+              placeholder="Username"
+              value={this.props.username}
+              onChange={this.props.onChange}
+              required
+              disabled={this.props.disabled}
+              style={{ marginBottom: '5px' }}
+            />
+            <button
+              type="submit"
+              className="btn btn-primary btn-block"
+              disabled={this.props.disabled}
+            >
+              Search
+            </button>
+
+            <button
+              type="button"
+              className="btn btn-secondary btn-block"
+              onClick={this.onCancelClick}
+            >
+              Cancel
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
 
 RegistrationSearchBox.propTypes = {
   alerts: PropTypes.array.isRequired,
--- app/js/profiles/components/registration/RegistrationSearchResults.js
+++ app/js/profiles/components/registration/RegistrationSearchResults.js
@@ -1,5 +1,5 @@
-import React, { PropTypes } from 'react'
-import { Link } from 'react-router'
+import React, { PropTypes, Component } from 'react'
+import { browserHistory } from 'react-router'
 import roundTo from 'round-to'
 
 const availabilityHeaderStyle = {
@@ -7,109 +7,167 @@ const availabilityHeaderStyle = {
   marginBottom: '0.5em'
 }
 
-const RegistrationSearchResults = (props) => (
-  <div>
-    <a
-      href=""
-      className="pull-left"
-      onClick={props.showSearchBox}
-    >
-      &lt; Back
-    </a>
-    <br />
+class RegistrationSearchResults extends Component {
+  static propTypes = {
+    showSearchBox: PropTypes.func.isRequired,
+    searchingUsername: PropTypes.string,
+    nameSuffixes: PropTypes.array.isRequired,
+    availableNames: PropTypes.object.isRequired,
+    ownerAddress: PropTypes.string.isRequired
+  }
 
-    <h3 className="modal-heading">Available names</h3>
+  constructor(props) {
+    super(props)
 
-    <div className="modal-body">
-      {props.searchingUsername ?
-        props.nameSuffixes.map((nameSuffix) => {
-          const name = `${props.searchingUsername}.${nameSuffix}`
-          const nameAvailabilityObject = props.availableNames[name]
-          const searching = !nameAvailabilityObject ||
-          nameAvailabilityObject.checkingAvailability
-          const isSubdomain = nameSuffix.split('.').length > 1
+    this.onRegisterFreeUserNameClick = this.onRegisterFreeUserNameClick.bind(this)
+    this.onBuyUserNameClick = this.onBuyUserNameClick.bind(this)
+  }
 
-          const available = nameAvailabilityObject &&
-            nameAvailabilityObject.available
-          const checkingPrice = nameAvailabilityObject &&
-            nameAvailabilityObject.checkingPrice
-          let price = 0
-          if (nameAvailabilityObject) {
-            price = nameAvailabilityObject.price
-          }
-          price = roundTo.up(price, 6)
+  onCancelClick() {
+    browserHistory.push('/profiles')
+  }
 
-          return (
-            <div key={nameSuffix}>
-            {searching ?
-              <h4>Checking {name}...</h4>
-              :
-              <div>
-                {available ?
-                  <div>
-                    <h4 style={availabilityHeaderStyle}>{name}</h4>
-                    {isSubdomain ?
-                      <Link
-                        className="btn btn-primary btn-sm"
-                        to={`/profiles/i/add-username/${props.ownerAddress}/select/${name}`}
-                      >
-                        Get <strong>{name}</strong> for free
-                      </Link>
-                    :
-                      <div>
-                      {checkingPrice ?
-                        <div className="progress">
-                          <div
-                            className="progress-bar progress-bar-striped progress-bar-animated"
-                            role="progressbar"
-                            aria-valuenow="100"
-                            aria-valuemin="0"
-                            aria-valuemax="100"
-                            style={{ width: '100%' }}
-                          >
-                          Checking price...
-                          </div>
-                        </div>
-                        :
-                        <Link
-                          className="btn btn-primary btn-sm"
-                          to={`/profiles/i/add-username/${props.ownerAddress}/select/${name}`}
-                        >
-                          Buy <strong>{name}</strong> for {price} bitcoins
-                        </Link>
-                      }
-                      </div>
-                    }
+  onRegisterFreeUserNameClick(name) {
+    browserHistory.push(`/profiles/i/add-username/${this.props.ownerAddress}/select/${name}`)
+  }
+
+  onBuyUserNameClick(name) {
+    browserHistory.push(`/profiles/i/add-username/${this.props.ownerAddress}/select/${name}`)
+  }
+
+  render() {
+    return (
+      <div>
+        <a
+          href=""
+          className="pull-left"
+          onClick={this.props.showSearchBox}
+        >
+          &lt; Back
+        </a>
+        <br />
+
+        <h3 className="modal-heading">Available names</h3>
+
+        <div className="modal-body">
+          {this.props.searchingUsername ?
+            this.props.nameSuffixes.map((nameSuffix) => {
+              const name = `${this.props.searchingUsername}.${nameSuffix}`
+              const nameAvailabilityObject = this.props.availableNames[name]
+              const searching = !nameAvailabilityObject ||
+              nameAvailabilityObject.checkingAvailability
+              const isSubdomain = nameSuffix.split('.').length > 1
+
+              const available = nameAvailabilityObject &&
+                nameAvailabilityObject.available
+              const checkingPrice = nameAvailabilityObject &&
+                nameAvailabilityObject.checkingPrice
+              let price = 0
+              if (nameAvailabilityObject) {
+                price = nameAvailabilityObject.price
+              }
+              price = roundTo.up(price, 6)
+
+              return (
+                <div key={nameSuffix}>
+                {searching ?
+                  <div className="username-check">
+                    <h4>Checking {name}...</h4>
+                    <button
+                      type="button"
+                      className="btn btn-secondary btn-block"
+                      onClick={this.onCancelClick}
+                    >
+                      Cancel
+                    </button>
                   </div>
                   :
                   <div>
-                    <h4 style={availabilityHeaderStyle}>{name}</h4>
-                    <button
-                      className="btn btn-primary btn-sm"
-                      disabled
-                    >
-                      {name} is already taken
-                    </button>
+                    {available ?
+                      <div>
+                        <h4 style={availabilityHeaderStyle}>{name}</h4>
+                        {isSubdomain ?
+                          <div className="username-result">
+                            <button
+                              className="btn btn-primary btn-block"
+                              onClick={() => this.onRegisterFreeUserNameClick(name)}
+                            >
+                              Get <strong>{name}</strong> for free
+                            </button>
+                            <button
+                              type="button"
+                              className="btn btn-secondary btn-block"
+                              onClick={this.onCancelClick}
+                            >
+                              Cancel
+                            </button>
+                          </div>
+                        :
+                          <div>
+                          {checkingPrice ?
+                            <div className="progress">
+                              <div
+                                className="progress-bar progress-bar-striped progress-bar-animated"
+                                role="progressbar"
+                                aria-valuenow="100"
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                                style={{ width: '100%' }}
+                              >
+                              Checking price...
+                              </div>
+                            </div>
+                            :
+                            <div className="username-result">
+                              <button
+                                className="btn btn-primary btn-block"
+                                onClick={() => this.onBuyUserNameClick(name)}
+                              >
+                                Buy <strong>{name}</strong> for {price} bitcoins
+                              </button>
+                              <button
+                                type="button"
+                                className="btn btn-secondary btn-block"
+                                onClick={this.onCancelClick}
+                              >
+                                Cancel
+                              </button>
+                            </div>
+
+                          }
+                          </div>
+                        }
+                      </div>
+                      :
+                      <div>
+                        <h4 style={availabilityHeaderStyle}>{name}</h4>
+                        <button
+                          className="btn btn-primary btn-block"
+                          disabled
+                        >
+                          {name} is already taken
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-secondary btn-block"
+                          onClick={this.onCancelClick}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    }
                   </div>
                 }
-              </div>
-            }
-            </div>
-          )
-        })
-        :
-        null
-      }
-    </div>
-  </div>
- )
-
-RegistrationSearchResults.propTypes = {
-  showSearchBox: PropTypes.func.isRequired,
-  searchingUsername: PropTypes.string,
-  nameSuffixes: PropTypes.array.isRequired,
-  availableNames: PropTypes.object.isRequired,
-  ownerAddress: PropTypes.string.isRequired
+                </div>
+              )
+            })
+            :
+            null
+          }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default RegistrationSearchResults
