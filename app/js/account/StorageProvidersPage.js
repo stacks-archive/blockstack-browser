ff17192d3f28d49ae8e7458e7c8a62005484353d
Larry Salibra <larry@appartisan.com>
2017-10-04 18:03:03 +0800

    clean up storage settings & address 'blinking' #730 #781

--- app/js/account/StorageProvidersPage.js
+++ app/js/account/StorageProvidersPage.js
@@ -13,8 +13,6 @@ import { connectToGaiaHub } from './utils/blockstack-inc'
 import { setCoreStorageConfig } from '../utils/api-utils'
 import log4js from 'log4js'
 
-import bitcoin from 'bitcoinjs-lib'
-
 const logger = log4js.getLogger('storage/StorageProvidersPage.js')
 
 const Dropbox = require('dropbox')
@@ -26,7 +24,8 @@ function mapStateToProps(state) {
     resetApi: PropTypes.func.isRequired,
     localIdentities: state.profiles.identity.localIdentities,
     identityKeypairs: state.account.identityAccount.keypairs,
-    connectedStorageAtLeastOnce: state.account.connectedStorageAtLeastOnce
+    connectedStorageAtLeastOnce: state.account.connectedStorageAtLeastOnce,
+    storageConnected: state.settings.api.storageConnected
   }
 }
 
@@ -42,41 +41,58 @@ class StorageProvidersPage extends Component {
     localIdentities: PropTypes.object.isRequired,
     identityKeypairs: PropTypes.array.isRequired,
     storageIsConnected: PropTypes.func.isRequired,
-    connectedStorageAtLeastOnce: PropTypes.bool.isRequired
+    connectedStorageAtLeastOnce: PropTypes.bool.isRequired,
+    storageConnected: PropTypes.bool.isRequired,
+    router: PropTypes.object.isRequired
   }
 
   constructor(props) {
     super(props)
-
+    this.state = {
+      hide: true
+    }
     this.connectDropbox = this.connectDropbox.bind(this)
     this.disconnectDropbox = this.disconnectDropbox.bind(this)
     this.updateApi = this.updateApi.bind(this)
   }
 
-  componentDidMount() {
+  componentWillMount() {
     const api = this.props.api
     const dropboxAccessToken = getDropboxAccessTokenFromHash(window.location.hash)
-    if (dropboxAccessToken != null) {
-      this.props.storageIsConnected()
-      const newApi = Object.assign({}, api, { dropboxAccessToken, storageConnected : true })
+    const needToConnectGaiaHub = window.location.hash === '#gaiahub'
+    const needToConnectDropbox = dropboxAccessToken != null
+    if (needToConnectDropbox) {
+      const newApi = Object.assign({}, api, { dropboxAccessToken })
       this.props.updateApi(newApi)
       setCoreStorageConfig(newApi)
       .then((indexUrl) => {
         logger.debug(`componentDidMount: indexUrl: ${indexUrl}`)
         // TODO add index URL to token file
         logger.debug('componentDidMount: storage initialized')
+        const newApi2 = Object.assign({}, newApi, { storageConnected: true })
+        this.props.updateApi(newApi2)
+        this.props.storageIsConnected()
+        logger.debug('componentDidMount: storage configured')
       })
     }
-    if (window.location.hash === "#gaiahub") {
+    if (needToConnectGaiaHub) {
       this.connectSharedService()
+    }
+
+    // We can show the page contents since there's not going to be a redirect
+    if (!(needToConnectGaiaHub || needToConnectDropbox)) {
+      this.setState({ hide: false })
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.api.dropboxAccessToken) {
+    if (nextProps.storageConnected) {
       if (!this.props.connectedStorageAtLeastOnce &&
         nextProps.connectedStorageAtLeastOnce) {
-        window.location = '/'
+        this.setState({
+          hide: true
+        })
+        this.props.router.push('/')
       }
     }
   }
@@ -96,15 +112,21 @@ class StorageProvidersPage extends Component {
     const storageProvider = this.props.api.gaiaHubUrl
     const signer = this.props.identityKeypairs[0].key
     connectToGaiaHub(storageProvider, signer)
-      .then( (gaiaHubConfig) => {
-        this.props.storageIsConnected()
+      .then((gaiaHubConfig) => {
         const newApi = Object.assign({}, this.props.api,
                                      { gaiaHubConfig,
-                                       storageConnected : true,
-                                       hostedDataLocation : BLOCKSTACK_INC })
+                                       hostedDataLocation: BLOCKSTACK_INC })
         this.props.updateApi(newApi)
         setCoreStorageConfig(newApi)
-        logger.debug('storage configured')
+        .then((indexUrl) => {
+          logger.debug(`componentDidMount: indexUrl: ${indexUrl}`)
+          // TODO add index URL to token file
+          logger.debug('componentDidMount: storage initialized')
+          const newApi2 = Object.assign({}, newApi, { storageConnected: true })
+          this.props.updateApi(newApi2)
+          this.props.storageIsConnected()
+          logger.debug('connectSharedService: storage configured')
+        })
       })
   }
 
@@ -119,67 +141,126 @@ class StorageProvidersPage extends Component {
 
   render() {
     const api = this.props.api
+    const hide = this.state.hide
     return (
-      <div className="m-b-100">
-        <h3 className="container-fluid m-t-10">
-          Storage Providers
-        </h3>
-        <p className="container-fluid">
-          Your profile and app data will be securely stored in the storage providers you connect.
-        </p>
-        {api.hostedDataLocation === DROPBOX ?
+      <div>
+      {hide ?
+      null
+      :
+        <div className="m-b-100">
+          <h3 className="container-fluid m-t-10">
+            Storage Providers
+          </h3>
+          <p className="container-fluid">
+            Your profile and app data will be securely stored in the storage provider you connect.
+          </p>
           <div className="container-fluid">
             <p>
-              {api.dropboxAccessToken == null ?
-                <button onClick={this.connectDropbox} className="btn btn-primary btn-storage btn-lg btn-block">
+              {api.hostedDataLocation !== BLOCKSTACK_INC ?
+                <button
+                  onClick={this.connectSharedService}
+                  className="btn btn-primary btn-storage btn-lg btn-block"
+                  disabled={this.props.storageIsConnected}
+                >
+                Connect default storage
+                </button>
+              :
+                <button
+                  onClick={this.disconnectSharedService}
+                  className="btn btn-primary btn-storage btn-lg btn-block"
+                  disabled
+                  title="Changing storage service providers will be supported in a future version."
+                >
+                Disconnect default storage
+                </button>
+              }
+            </p>
+            <p>
+              {api.hostedDataLocation !== DROPBOX ?
+                <button
+                  onClick={this.connectDropbox}
+                  className="btn btn-primary btn-storage btn-lg btn-block"
+                  disabled={this.props.storageIsConnected}
+                >
                 Connect Dropbox
                 </button>
               :
                 <button
                   onClick={this.disconnectDropbox}
                   className="btn btn-primary btn-storage btn-lg btn-block"
+                  title="Changing storage service providers will be supported in a future version."
+                  disabled
                 >
                 Disconnect Dropbox
                 </button>
               }
             </p>
             <p>
-              <button disabled className="btn btn-primary btn-storage btn-lg btn-block" title="Coming soon!">
+              <button
+                disabled
+                className="btn btn-primary btn-storage btn-lg btn-block"
+                title="Coming soon!"
+              >
                 Connect IPFS
               </button>
             </p>
             <p>
-              <button disabled className="btn btn-primary btn-storage btn-lg btn-block" title="Coming soon!">
+              <button
+                disabled
+                className="btn btn-primary btn-storage btn-lg btn-block"
+                title="Coming soon!"
+              >
+              Connect self-hosted storage
+              </button>
+            </p>
+            <p>
+              <button
+                disabled
+                className="btn btn-primary btn-storage btn-lg btn-block"
+                title="Coming soon!"
+              >
                 Connect Sia
               </button>
             </p>
             <p>
-              <button disabled className="btn btn-primary btn-storage btn-lg btn-block" title="Coming soon!">
+              <button
+                disabled
+                className="btn btn-primary btn-storage btn-lg btn-block"
+                title="Coming soon!"
+              >
                 Connect Storj
               </button>
             </p>
             <p>
-              <button disabled className="btn btn-primary btn-storage btn-lg btn-block" title="Coming soon!">
+              <button
+                disabled
+                className="btn btn-primary btn-storage btn-lg btn-block"
+                title="Coming soon!"
+              >
                 Connect Google Drive
               </button>
             </p>
             <p>
-              <button disabled className="btn btn-primary btn-storage btn-lg btn-block" title="Coming soon!">
+              <button
+                disabled
+                className="btn btn-primary btn-storage btn-lg btn-block"
+                title="Coming soon!"
+              >
                 Connect Amazon Cloud Drive
               </button>
             </p>
             <p>
-              <button disabled className="btn btn-primary btn-storage btn-lg btn-block" title="Coming soon!">
+              <button
+                disabled
+                className="btn btn-primary btn-storage btn-lg btn-block"
+                title="Coming soon!"
+              >
               Connect Microsoft OneDrive
               </button>
             </p>
-            <p>
-              <button disabled className="btn btn-primary btn-storage btn-lg btn-block" title="Coming soon!">
-              Connect Self-hosted Drive
-              </button>
-            </p>
           </div>
-        : null}
+        </div>
+      }
       </div>
     )
   }
