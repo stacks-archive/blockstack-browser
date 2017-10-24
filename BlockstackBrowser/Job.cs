f394e2d751566182b07cdf1210d6d3586cd4a3ba
Aaron Blankstein <aaron@blockstack.com>
2017-10-24 11:38:39 -0700

    adding installer

---
+++ BlockstackBrowser/Job.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace BlockstackBrowser
+{
+    //  http://stackoverflow.com/a/4232259/386091
+    //  http://stackoverflow.com/a/9507862/386091
+
+    public static class ChildProcessTracker
+    {
+        public static void AddProcess(Process process)
+        {
+            if (s_jobHandle != IntPtr.Zero)
+            {
+                bool success = AssignProcessToJobObject(s_jobHandle, process.Handle);
+                if (!success)
+                    throw new Win32Exception();
+            }
+        }
+
+        static ChildProcessTracker()
+        {
+            if (Environment.OSVersion.Version < new Version(6, 2))
+                return;
+
+            string jobName = "ChildProcessTracker" + Process.GetCurrentProcess().Id;
+            s_jobHandle = CreateJobObject(IntPtr.Zero, jobName);
+
+            var info = new JOBOBJECT_BASIC_LIMIT_INFORMATION();
+
+            info.LimitFlags = JOBOBJECTLIMIT.JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE;
+
+            var extendedInfo = new JOBOBJECT_EXTENDED_LIMIT_INFORMATION();
+            extendedInfo.BasicLimitInformation = info;
+
+            int length = Marshal.SizeOf(typeof(JOBOBJECT_EXTENDED_LIMIT_INFORMATION));
+            IntPtr extendedInfoPtr = Marshal.AllocHGlobal(length);
+            try
+            {
+                Marshal.StructureToPtr(extendedInfo, extendedInfoPtr, false);
+
+                if (!SetInformationJobObject(s_jobHandle, JobObjectInfoType.ExtendedLimitInformation,
+                    extendedInfoPtr, (uint)length))
+                {
+                    throw new Win32Exception();
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(extendedInfoPtr);
+            }
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        static extern IntPtr CreateJobObject(IntPtr lpJobAttributes, string name);
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetInformationJobObject(IntPtr job, JobObjectInfoType infoType,
+            IntPtr lpJobObjectInfo, uint cbJobObjectInfoLength);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AssignProcessToJobObject(IntPtr job, IntPtr process);
+
+        private static readonly IntPtr s_jobHandle;
+    }
+
+    public enum JobObjectInfoType
+    {
+        AssociateCompletionPortInformation = 7,
+        BasicLimitInformation = 2,
+        BasicUIRestrictions = 4,
+        EndOfJobTimeInformation = 6,
+        ExtendedLimitInformation = 9,
+        SecurityLimitInformation = 5,
+        GroupInformation = 11
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct JOBOBJECT_BASIC_LIMIT_INFORMATION
+    {
+        public Int64 PerProcessUserTimeLimit;
+        public Int64 PerJobUserTimeLimit;
+        public JOBOBJECTLIMIT LimitFlags;
+        public UIntPtr MinimumWorkingSetSize;
+        public UIntPtr MaximumWorkingSetSize;
+        public UInt32 ActiveProcessLimit;
+        public Int64 Affinity;
+        public UInt32 PriorityClass;
+        public UInt32 SchedulingClass;
+    }
+
+    [Flags]
+    public enum JOBOBJECTLIMIT : uint
+    {
+        JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE = 0x2000
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct IO_COUNTERS
+    {
+        public UInt64 ReadOperationCount;
+        public UInt64 WriteOperationCount;
+        public UInt64 OtherOperationCount;
+        public UInt64 ReadTransferCount;
+        public UInt64 WriteTransferCount;
+        public UInt64 OtherTransferCount;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct JOBOBJECT_EXTENDED_LIMIT_INFORMATION
+    {
+        public JOBOBJECT_BASIC_LIMIT_INFORMATION BasicLimitInformation;
+        public IO_COUNTERS IoInfo;
+        public UIntPtr ProcessMemoryLimit;
+        public UIntPtr JobMemoryLimit;
+        public UIntPtr PeakProcessMemoryUsed;
+        public UIntPtr PeakJobMemoryUsed;
+    }
+}
--- BlockstackBrowser/Program.cs
+++ BlockstackBrowser/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
 
 namespace BlockstackBrowser
 {
@@ -12,9 +14,6 @@ namespace BlockstackBrowser
         ContextMenu contextMenu;
         Process browserProxy, corsProxy;
 
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
@@ -23,13 +22,21 @@ namespace BlockstackBrowser
             Application.Run(new Program());
         }
 
+        static void deleteAndUnpackFiles()
+        {
+            if (!Directory.Exists("Resources\\browser-build"))
+                ZipFile.ExtractToDirectory("Resources\\browser-build.zip", "Resources");
+            if (!Directory.Exists("Resources\\corsproxy-https"))
+                ZipFile.ExtractToDirectory("Resources\\corsproxy-https.zip", "Resources");
+        }
+
         public Program()
         {
             container = new System.ComponentModel.Container();
 
             myIcon = new NotifyIcon(container);
             myIcon.Visible = true;
-            myIcon.Icon = new System.Drawing.Icon("blockstack.ico");
+            myIcon.Icon = new System.Drawing.Icon("Resources\\blockstack.ico");
 
             myIcon.BalloonTipText = "Blockstack Browser";
             myIcon.Text = "Blockstack Browser";
@@ -37,17 +44,23 @@ namespace BlockstackBrowser
             contextMenu = new ContextMenu();
 
             MenuItem exitItem = new MenuItem();
-            exitItem.Index = 0;
+            exitItem.Index = 1;
             exitItem.Text = "E&xit";
             exitItem.Click += new System.EventHandler(this.exitRequest);
             MenuItem homeItem = new MenuItem();
+            homeItem.Index = 0;
+            homeItem.Text = "H&ome";
+            homeItem.Click += new System.EventHandler(this.homeOpen);
 
             contextMenu.MenuItems.AddRange(
-                new MenuItem[] { exitItem, homeItem });
+                new MenuItem[] { homeItem, exitItem });
             myIcon.ContextMenu = contextMenu;
+
+            //deleteAndUnpackFiles();
 
             RunBlockstackBrowser();
             RunCORSProxy();
+            this.homeOpen(null, null);
         }
 
         protected override void SetVisibleCore(bool value)
@@ -62,6 +75,11 @@ namespace BlockstackBrowser
         private void exitRequest(object Sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void homeOpen(object Sender, EventArgs e)
+        {
+            Process.Start("http://localhost:8888/");
         }
 
         protected override void Dispose(bool disposing)
@@ -84,22 +102,25 @@ namespace BlockstackBrowser
             Process process = new Process();
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.FileName = "node.exe";
+            startInfo.FileName = "Resources\\node.exe";
             startInfo.Arguments = command;
             process.StartInfo = startInfo;
             process.Start();
+
+            ChildProcessTracker.AddProcess(process);
+
             return process;
         }
 
         private void RunBlockstackBrowser()
         {
-            String blockstackCommand = "blockstackProxy.js 8888 browser-build";
+            String blockstackCommand = "Resources\\blockstackProxy.js 8888 Resources\\browser-build";
             this.browserProxy = ShellOut(blockstackCommand);
         }
 
         private void RunCORSProxy()
         {
-            String proxyCommand = "corsproxy-https\\node_modules\\corsproxy-https\\bin\\corsproxy";
+            String proxyCommand = "Resources\\corsproxy-https\\node_modules\\corsproxy-https\\bin\\corsproxy";
             this.corsProxy = ShellOut(proxyCommand);
         }
     }
