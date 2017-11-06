6e3797d08c5f3788037afdac671301b8e015449b
Aaron Blankstein <aaron@blockstack.com>
2017-11-05 19:26:36 -0500

    slightly friendlier landing page

--- app/js/welcome/components/WebAppWarningView.js
+++ app/js/welcome/components/WebAppWarningView.js
@@ -15,12 +15,13 @@ const WebAppPage = (props) =>
         marginTop: '10px'
       }}
     />
-    <h3 className="modal-heading p-b-25">This simple app allows you to explore Blockstack and <br />
-    create an ID.
+    <h3 className="modal-heading p-b-25">
+      This simple app allows you to explore Blockstack.
     </h3>
-    <h3 className="modal-heading p-b-25">To experience the full feature set of the decentralized internet, <br />
-    download the Blockstack app.
-    </h3>
+    <p className="modal-body">
+      To experience the full feature set of the decentralized internet, <br />
+      download the Blockstack app.
+    </p>
     <div className="m-t-30 m-b-20 modal-body">
       <a className="btn btn-primary btn-block m-b-10" href="http://blockstack.org/install">
         Download Blockstack
