fc79be1136ed17fe3fd4fc7230e13fadc29d86e2
Aaron Blankstein <aaron@blockstack.com>
2017-11-07 18:11:22 -0500

    remove tokensale icon from webapp

--- app/js/HomeScreenPage.js
+++ app/js/HomeScreenPage.js
@@ -92,12 +92,6 @@ class HomeScreenPage extends Component {
                       return null
                     } }
                   )}
-                  <AppIcon
-                    key="token-sale"
-                    iconImage="app-icon-token-sale.png"
-                    displayName="Token Sale"
-                    launchLink="https://blockstack.com/"
-                  />
                 </div>
               </div>
 
