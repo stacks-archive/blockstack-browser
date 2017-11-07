3dd1276d647075465824247a5e701f43fe56fe51
Aaron Blankstein <aaron@blockstack.com>
2017-11-07 18:13:20 -0500

    add *back* the tokensale icon

--- app/js/HomeScreenPage.js
+++ app/js/HomeScreenPage.js
@@ -112,6 +112,12 @@ class HomeScreenPage extends Component {
                       return null
                     } }
                   )}
+                  <AppIcon
+                    key="token-sale"
+                    iconImage="app-icon-token-sale.png"
+                    displayName="Token Sale"
+                    launchLink="https://blockstack.com/"
+                  />
                 </div>
               </div>
 
