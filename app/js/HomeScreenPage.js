aa03c21b0f0e8c7be20d0ba7f9f8f69cdba43927
Ken <yukanliao@gmail.com>
2017-11-08 14:06:25 -0500

    Add back token sale app icon. #1034

--- app/js/HomeScreenPage.js
+++ app/js/HomeScreenPage.js
@@ -108,6 +108,12 @@ class HomeScreenPage extends Component {
                       return null
                     } }
                   )}
+                  <AppIcon
+                    key="token-sale"
+                    iconImage="app-icon-token-sale.png"
+                    displayName="Token Sale"
+                    launchLink="https://blockstack.com/"
+                  />
                 </div>
               </div>
 
