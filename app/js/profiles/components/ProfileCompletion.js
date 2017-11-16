7df2017fc2ade458ebe32b4218e93221a0bd70b2
Guy Lepage <guylepage3@gmail.com>
2017-11-15 19:43:09 -0500

    new profile completion instructions blockstackinc/blockstack-dot-com#459

--- app/js/profiles/components/ProfileCompletion.js
+++ app/js/profiles/components/ProfileCompletion.js
@@ -9,12 +9,13 @@ class ProfileCompletion extends Component {
   render() {
     const pct = this.props.completePct*100
     return (
-      <div className="container-fluid p-0">
+      <div className="container-fluid p-0 m-t-25">
         <div className="row">
           <div className="col">
             <div className="profile-completion m-b-20">
-              <div className="m-b-10">
-                <span className="font-weight-bold">Complete your ID</span>
+              <div className="m-b-30">
+                <h2 className="font-weight-bold text-light">Step 2</h2>
+                <p className="text-light">Complete your Blockstack ID by adding your name and proving at least one social account.</p>
               </div>
               <div className="progress">
                 <div
@@ -25,14 +26,16 @@ class ProfileCompletion extends Component {
                   aria-valuemin="0"
                   aria-valuemax="100"
                 >
-                  <span className={pct === 0 ? 'progress-bar-zero' : ''}>
+                  <span className={pct === 0 ? 'progress-bar-zero font-weight-bold' : ''}>
                     {`${pct}%`}
                   </span>
                 </div>
               </div>
-              <div className="profile-completion-sub m-t-10">
-                Complete your Blockstack ID by adding your name and a verified social account.
+              <div className="profile-completion-sub m-t-30 text-light">
+                Once you've completed your Blockstack ID by adding your name and proving your social account, you'll be able to register for the Blockstack Token Sale Voucher.
               </div>
+              <a className="btn btn-block disabled btn-primary-disabled m-t-30 m-b-15" href="/">Login with Blockstack</a>
+              <a className="btn btn-light-primary btn-block m-t-30 m-b-15" href="/">Login with Blockstack</a>
             </div>
           </div>
         </div>
