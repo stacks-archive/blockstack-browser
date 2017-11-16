686ce92d5f0ed81e3229ed501c2ac3a9f502d3a6
Ken <yukanliao@gmail.com>
2017-11-16 18:56:27 -0500

    Continue to token registration on profile progress.

--- app/js/profiles/components/ProfileCompletion.js
+++ app/js/profiles/components/ProfileCompletion.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
+const registrationUrl = 'https://www.blockstack.com/register-from-blockstack-browser'
+
 class ProfileCompletion extends Component {
   static propTypes = {
     completePct: PropTypes.number.isRequired,
@@ -14,11 +16,14 @@ class ProfileCompletion extends Component {
           <div className="col">
             <div className="profile-completion m-b-20">
               <div className="m-b-10">
-                <span className="font-weight-bold">Complete your ID</span>
+                <h3>Step 2: Complete Your Profile</h3>
               </div>
-              <div className="progress">
+              <div className="profile-completion-sub m-t-10 m-b-20">
+                Complete your Blockstack ID by adding your name and a verified social account.
+              </div>
+              <div className="progress progress-white">
                 <div
-                  className="progress-bar progress-bar-blue"
+                  className="progress-bar progress-bar-green"
                   role="progressbar"
                   style={{width: `${pct}%`}}
                   aria-valuenow={`${pct}`}
@@ -30,9 +35,17 @@ class ProfileCompletion extends Component {
                   </span>
                 </div>
               </div>
-              <div className="profile-completion-sub m-t-10">
-                Complete your Blockstack ID by adding your name and a verified social account.
+              <div className="profile-completion-sub m-t-20 m-b-20">
+                Once complete, you'll be able to register 
+                for the Blockstack Token Sale Voucher Wait List.
               </div>
+              {pct === 100 ?
+                <a href={registrationUrl} className="btn btn-sm btn-inverse btn-block">Continue to Wait List</a>
+                :
+                <button type="button" className="btn btn-sm btn-inverse btn-block btn-inverse-disabled">
+                  Continue to Wait List
+                </button>
+              }
             </div>
           </div>
         </div>
