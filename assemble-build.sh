84cab0fe183b0746525eaeda1d2d103d7ec16bd4
Aaron Blankstein <aaron@blockstack.com>
2017-12-19 17:21:48 -0500

    add script for making browser build dir

---
+++ assemble-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf blockstack-browser
+
+git clone https://github.com/blockstack/blockstack-browser
+cd blockstack-browser
+
+npm i
+
+NODE_ENV=production gulp prod-windows
+
+cp -r build ../BlockstackBrowser/Resources/build
