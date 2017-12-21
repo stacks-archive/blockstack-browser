5625b12953874447e38b06d80a3f3bb38c676dd2
Aaron Blankstein <aaron@blockstack.com>
2017-12-21 10:09:49 -0500

    match version in the built browser tag to the wix installer version

--- assemble-build.sh
+++ assemble-build.sh
@@ -2,11 +2,16 @@
 
 rm -rf blockstack-browser
 
+VERSION=0.21.5
+
 git clone https://github.com/blockstack/blockstack-browser
 cd blockstack-browser
+git checkout "v$VERSION"
 
 npm i
 
 NODE_ENV=production gulp prod-windows
 
-#cp -r build ../BlockstackBrowser/Resources/build
+cp -r build ../BlockstackBrowser/Resources/build
+
+sed -i -e "s/Version=\"0.0.0.0\"/Version=\"$VERSION\"/" BlockstackSetup/Product.wxs
