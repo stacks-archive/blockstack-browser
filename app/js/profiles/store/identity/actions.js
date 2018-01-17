999081e098111eb95c9917f1d2ab5676e53a80ee
Aaron Blankstein <aaron@blockstack.com>
2018-01-17 11:52:26 -0500

    intelligent reads. this passes tests, but that is misleading, because there should be more test-coverage in this spot

--- app/js/profiles/store/identity/actions.js
+++ app/js/profiles/store/identity/actions.js
@@ -209,6 +209,61 @@ function createNewProfile(encryptedBackupPhrase: string,
 }
 
 /**
+ * Try to fetch and verify a profile from the historic set of default locations,
+ * in order of recency. If all of them return 404s, or fail to validate, return null
+ */
+function fetchProfileLocations(gaiaUrlBase: string,
+                               ownerAddress: string,
+                               firstAddress: string,
+                               ownerIndex: number) {
+  function recursiveTryFetch(locations) {
+    if (locations.length === 0) {
+      return Promise.resolve(null)
+    }
+    const location = locations[0]
+    return fetch(location)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+            .then(tokenRecords => getProfileFromTokens(tokenRecords, ownerAddress))
+            .then(profile => {
+              if (profile) {
+                logger.debug(`Found valid profile at ${location}`)
+                return profile
+              } else {
+                logger.debug(`Failed to verify profile at ${location}... trying others`)
+                return recursiveTryFetch(locations.slice(1))
+              }
+            })
+        } else {
+          logger.debug(`Failed to find profile at ${location}... trying others`)
+          return recursiveTryFetch(locations.slice(1))
+        }
+      })
+  }
+
+  const urls = []
+  // the new default
+  urls.push(`${gaiaUrlBase}/${ownerAddress}/profile.json`)
+
+  // the 'indexed' URL --
+  //  this is gaia/:firstAddress/:index/profile.json
+  //  however, the index is _not_ equal to the current index.
+  //  indexes were mapped from
+  //    correct: [0, 1, 3, 5, 7, 9...]
+  //  incorrect: [0, 1, 2, 3, 4, 5...]
+
+  if (ownerIndex < 2) {
+    urls.push(`${gaiaUrlBase}/${firstAddress}/${ownerIndex}/profile.json`)
+  } else if (ownerIndex % 2 === 1) {
+    const buggedIndex = 1 + Math.floor(ownerIndex / 2)
+    urls.push(`${gaiaUrlBase}/${firstAddress}/${buggedIndex}/profile.json`)
+  }
+
+  return recursiveTryFetch(urls)
+}
+
+/**
  * Checks each owner address to see if it owns a name, if it owns a name,
  * it resolves the profile and updates the state with the owner address's
  * current name.
@@ -230,47 +285,31 @@ function refreshIdentities(api: {bitcoinAddressLookupUrl: string,
         .then((responseText) => JSON.parse(responseText))
         .then((responseJson) => {
           if (responseJson.names.length === 0) {
-            logger.debug(`refreshIdentities: ${address} owns no names`)
+            logger.debug(`refreshIdentities: ${address} owns no names, checking default locations.`)
             const gaiaBucketAddress = ownerAddresses[0]
-            const profileUrlBase = `https://gaia.blockstack.org/hub/${gaiaBucketAddress}`
-            const profileUrl = `${profileUrlBase}/${index}/profile.json`
-            logger.debug(`refreshIdentities: check default storage for profile: ${profileUrl}`)
-            return fetch(profileUrl)
-            .then(response => {
-              if (response.ok) {
-                response.text()
-                .then(responseText => JSON.parse(responseText))
-                .then((responseJsonProfile) => {
-                  const tokenRecords = responseJsonProfile
-                  const profile = getProfileFromTokens(tokenRecords, address)
-                  if (profile) {
-                    logger.debug(`refreshIdentities: found profile at ${profileUrl}`)
-                    const zoneFile = ''
-                    dispatch(updateProfile(index, profile, zoneFile))
-                    let verifications = []
-                    let trustLevel = 0
-                    logger.debug(`refreshIdentities: validating address proofs for ${address}`)
-                    return validateProofsService(profile, address).then((proofs) => {
-                      verifications = proofs
-                      trustLevel = calculateTrustLevel(verifications)
-                      dispatch(updateSocialProofVerifications(index, verifications, trustLevel))
-                    })
+            return fetchProfileLocations('https://gaia.blockstack.org/hub',
+                                         address, gaiaBucketAddress, index)
+              .then(profile => {
+                if (profile) {
+                  const zoneFile = ''
+                  dispatch(updateProfile(index, profile, zoneFile))
+                  let verifications = []
+                  let trustLevel = 0
+                  logger.debug(`refreshIdentities: validating address proofs for ${address}`)
+                  return validateProofsService(profile, address).then((proofs) => {
+                    verifications = proofs
+                    trustLevel = calculateTrustLevel(verifications)
+                    dispatch(updateSocialProofVerifications(index, verifications, trustLevel))
+                  })
                     .catch((error) => {
                       logger.error(`refreshIdentities: ${address} validateProofs: error`, error)
                       return Promise.resolve()
                     })
-                  } else {
-                    resolve()
-                    return Promise.resolve()
-                  }
-                })
-                return Promise.resolve()
-              } else {
-                logger.debug(`refreshIdentities: nothing found in default storage at ${profileUrl}`)
-                resolve()
-                return Promise.resolve()
-              }
-            })
+                } else {
+                  resolve()
+                  return Promise.resolve()
+                }
+              })
           } else {
             if (responseJson.names.length === 1) {
               logger.debug(`refreshIdentities: ${address} has 1 name}`)
