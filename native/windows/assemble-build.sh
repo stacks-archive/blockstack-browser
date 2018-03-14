cfefc75b0024922bf6f756ab39cc793484e562aa
Aaron Blankstein <aaron@blockstack.com>
2018-03-14 11:49:15 -0400

    fetch node.exe and copy blockstackProxy dynamically

--- native/windows/assemble-build.sh
+++ native/windows/assemble-build.sh
@@ -9,5 +9,7 @@ npm i
 NODE_ENV=production gulp prod-windows
 
 cp -r build native/windows/BlockstackBrowser/Resources/build
+cp native/blockstackProxy.js native/windows/BlockstackBrowser/Resources/blockstackProxy.js
+curl https://nodejs.org/dist/latest-v8.x/win-x86/node.exe --output native/windows/BlockstackBrowser/Resources/node.exe
 
 sed -i -e "s/Version=\"0.0.0.0\"/Version=\"$VERSION\"/" native/windows/BlockstackSetup/Product.wxs
