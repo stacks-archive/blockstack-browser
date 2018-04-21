2e31b992ac0b3d02a29c18a5fa1239d5a2d7e853
jeff christian <15chrjef@gmail.com>
2018-04-20 18:07:38 -0700

    (fix) linting on ActionItem component

--- app/js/components/ActionItem.js
+++ app/js/components/ActionItem.js
@@ -1,29 +1,27 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import { Link } from 'react-router'
+import React from 'react'
 
-class ActionItem extends Component {
-  static propTypes = {
-    action: PropTypes.string.isRequired,
-    destinationUrl: PropTypes.string.isRequired,
-    destinationName: PropTypes.string.isRequired,
-    completed: PropTypes.bool.isRequired
+const ActionItem = () => (
+  <div>
+  {this.props.completed ? null
+  :
+    <div className="action-item">
+      <div>{this.props.action}</div>
+      <div><small>{this.props.detail}</small></div>
+      <a className="tooltip-link" href={this.props.destinationUrl}> 
+        › Go to {this.props.destinationName
+      }</a>
+    </div>
   }
+  </div>
+)
 
-  render() {
-    return (
-      <div>
-      {this.props.completed ? null
-      :
-        <div className="action-item">
-          <div>{this.props.action}</div>
-          <div><small>{this.props.detail}</small></div>
-          <a className="tooltip-link" href={this.props.destinationUrl}> › Go to {this.props.destinationName}</a>
-        </div>
-      }
-      </div>
-    )
-  }
+
+ActionItem.PropTypes = {
+  action: PropTypes.string.isRequired,
+  destinationUrl: PropTypes.string.isRequired,
+  destinationName: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired
 }
 
 export default ActionItem
