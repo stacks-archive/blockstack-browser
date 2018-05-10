cc54c28427b3d8e98a7f99273346bbf69e20d842
hank <hstove@gmail.com>
2018-05-10 06:28:13 -0700

    query username from core.blockstack, related to  #1327

--- app/js/onboarding/views/Username.js
+++ app/js/onboarding/views/Username.js
@@ -14,14 +14,10 @@ const getUsernameStatus = async (username, sponsoredName = '.personal.id') => {
   if (!username) {
     return null
   }
-  // const url = `https://core.blockstack.org/v1/names/${username.toLowerCase()}${sponsoredName}`
-  const url = `https://registrar.blockstack.org/v1/names/${username.toLowerCase()}${sponsoredName}`
+  const url = `https://core.blockstack.org/v1/names/${username.toLowerCase()}${sponsoredName}`
   const res = await fetch(url)
   const user = await res.json()
   logger.debug('got response', user)
-  if (user.error === 'No such subdomain') {
-    return 'available'
-  }
   return user.status
 }
 const validationSchema = Yup.object({
