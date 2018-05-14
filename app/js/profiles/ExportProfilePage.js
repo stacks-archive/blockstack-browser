eba9d0e801dc5512c856c79c9841075359fee793
Matt Davis <matt.davis@mediacurrent.com>
2018-05-13 17:52:59 -0700

    lint fixes for app/js/profiles/ExportProfilePage.js

--- app/js/profiles/ExportProfilePage.js
+++ app/js/profiles/ExportProfilePage.js
@@ -1,31 +1,22 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 
 import InputGroup from '../components/InputGroup'
 import PageHeader from '../components/PageHeader'
 
-class ExportPage extends Component {
-  constructor() {
-    super()
-  }
-
-  render() {
-    return (
-      <div className="body-inner body-inner-white">
-        <PageHeader title="Export" />
-        <div className="container">
-          <div>
-            <h3>Export Identity</h3>
-            <p>Export your identity to another address.</p>
-            <InputGroup label="Recipient address" placeholder="Recipient address" />
-            <div>
-              <button className="btn btn-primary">Send</button>
-            </div>
-          </div>
+const ExportPage = () => (
+  <div className="body-inner body-inner-white">
+    <PageHeader title="Export" />
+    <div className="container">
+      <div>
+        <h3>Export Identity</h3>
+        <p>Export your identity to another address.</p>
+        <InputGroup label="Recipient address" placeholder="Recipient address" />
+        <div>
+          <button className="btn btn-primary">Send</button>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  </div>
+)
 
 export default ExportPage
