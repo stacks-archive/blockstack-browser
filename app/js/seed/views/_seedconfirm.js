0a41d50a9a72d6ab1551c893adbe9704db0d4d9a
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-05-29 13:09:52 -0500

    seed recovery confirm improved

--- app/js/seed/views/_seedconfirm.js
+++ app/js/seed/views/_seedconfirm.js
@@ -2,14 +2,48 @@ import React from 'react'
 import { ShellScreen, Type, Button, Buttons } from '@blockstack/ui'
 import PropTypes from 'prop-types'
 
+function getRandomInt(min, max, exclude) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min
+  if (exclude === randomNumber) {
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  return randomNumber
+}
+
 class SeedConfirm extends React.Component {
+  generateNumbers = () => {
+    const firstNumber = getRandomInt(0, 11)
+    const secondNumber = getRandomInt(0, 11, firstNumber)
+
+    this.setState({
+      indexesToConfirm: [firstNumber, secondNumber],
+      wordsToConfirm: [
+        this.props.seed[firstNumber],
+        this.props.seed[secondNumber]
+      ]
+    })
+  }
+
   state = {
-    wordsToConfirm: [this.props.seed[7], this.props.seed[2]],
-    verified: false
+    indexesToConfirm: [],
+    wordsToConfirm: [],
+    selectedWords: [],
+    words: {},
+    confirmedWords: [],
+    randomWords: [],
+    verified: false,
+    loading: false,
+    error: false
   }
 
   componentWillMount() {
-    if (!this.state.randomWords) {
+    if (!this.state.indexesToConfirm.length) {
+      this.generateNumbers()
+    }
+    if (!this.state.randomWords.length) {
       this.randomWords()
     }
   }
@@ -32,35 +66,88 @@ class SeedConfirm extends React.Component {
     ) {
       this.setVerified()
     }
+    if (!this.state.indexesToConfirm.length) {
+      this.generateNumbers()
+    }
+    if (!this.state.randomWords.length) {
+      this.randomWords()
+    }
   }
 
-  checkWord = word => {
-    if (this.state.wordsToConfirm.find(w => w === word)) {
-      this.setState({
-        [word]: true
-      })
+  handleWordClick = word => {
+    if (this.state.selectedWords.length < 2) {
+      this.setState(
+        state => ({
+          ...state,
+          words: { ...state.words, [word]: true },
+          selectedWords: [...state.selectedWords, word]
+        }),
+        () => {
+          if (this.state.wordsToConfirm.find(w => w === word)) {
+            this.validWord(word)
+          }
+        }
+      )
     }
+  }
+
+  resetState = () => {
+    this.setState({
+      indexesToConfirm: [],
+      wordsToConfirm: [],
+      selectedWords: [],
+      confirmedWords: [],
+      randomWords: [],
+      words: {},
+      verified: false,
+      loading: false,
+      error: true
+    })
+  }
+
+  handleSubmit = () => {
+    this.setState({ loading: true }, () =>
+      setTimeout(() => {
+        if (this.state.confirmedWords.length !== 2) {
+          this.resetState()
+        } else {
+          this.props.next()
+          this.props.setVerified()
+          this.setState({
+            loading: false
+          })
+        }
+      }, 1500)
+    )
+  }
+
+  validWord = word => {
+    this.setState(state => ({
+      confirmedWords: [...state.confirmedWords, word]
+    }))
   }
 
   randomWords = () => {
     const { seed } = this.props
     const randomWords = [...seed].sort(() => 0.5 - Math.random())
-    if (!this.state.randomWords) {
+    if (!this.state.randomWords.length) {
       this.setState({
         randomWords
       })
     }
   }
   render() {
-    const { next, seed, verified, ...rest } = this.props
-
+    const { seed, ...rest } = this.props
+    if (!seed || !this.state.randomWords.length) {
+      return null
+    }
     const renderWords = (words, action) =>
       this.state.randomWords.map(word => (
         <Button
           label={word}
           key={word}
           onClick={() => action(word)}
-          filled={this.state[word]}
+          filled={this.state.words[word]}
         />
       ))
 
@@ -75,8 +162,13 @@ class SeedConfirm extends React.Component {
         grow: 1,
         children: (
           <React.Fragment>
-            <Type.h3>Select words #1 and #8</Type.h3>
-            <Buttons wrap>{renderWords(seed, w => this.checkWord(w))}</Buttons>
+            <Type.h3>
+              Select words #{this.state.indexesToConfirm[0] + 1} and #{this
+                .state.indexesToConfirm[1] + 1}
+            </Type.h3>
+            <Buttons wrap>
+              {renderWords(seed, w => this.handleWordClick(w))}
+            </Buttons>
           </React.Fragment>
         )
       },
@@ -88,13 +180,11 @@ class SeedConfirm extends React.Component {
             textOnly: true
           },
           {
-            label: 'Next',
+            label: this.state.error ? 'Try Again' : 'Confirm',
             primary: true,
-            icon: 'ArrowRightIcon',
-            disabled: !verified,
-            onClick: () => {
-              next()
-            }
+            disabled: !this.state.selectedWords === 2,
+            loading: this.state.loading,
+            onClick: () => this.handleSubmit()
           }
         ]
       }
