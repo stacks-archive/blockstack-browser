5f7c0b72b63d3f7c9e1b9379eb9303af64072166
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-05-30 11:33:23 -0500

    move sign up back under app nested view

--- app/js/routes.js
+++ app/js/routes.js
@@ -54,6 +54,8 @@ const accountCreated = connectedRouterRedirect({
 export default (
   <Router history={browserHistory}>
     <Route path="/" component={accountCreated(App)}>
+      <Route path="/sign-up" component={SignUpPage} />
+      <Route path="/sign-up/*" component={SignUpPage} />
       <IndexRoute component={HomeScreenPage} />
       <Route path="profiles" component={ProfilesApp}>
         <IndexRoute component={DefaultProfilePage} />
@@ -103,8 +105,6 @@ export default (
      * because when we wipe data, it wants to redirect to /sign-up
      */}
     <Route path="/update" component={UpdateStatePage} />
-    <Route path="/sign-up" component={SignUpPage} />
-    <Route path="/sign-up/*" component={SignUpPage} />
     <Route path="/sign-in" component={SignInPage} />
     <Route path="/sign-in/*" component={SignInPage} />
     <Route path="/seed" component={SeedPage} />
