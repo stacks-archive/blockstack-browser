a38ec800595337b7174280cd0b7fb29b0e1d2ec4
Matt Davis <matt.davis@mediacurrent.com>
2018-06-02 14:35:13 -0700

    Eslint cleanup for ProfileCompletion component

--- app/js/profiles/components/ProfileCompletion.js
+++ app/js/profiles/components/ProfileCompletion.js
@@ -1,45 +1,42 @@
+import React from 'react'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import { Link } from 'react-router'
 
-class ProfileCompletion extends Component {
-  static propTypes = {
-    completePct: PropTypes.number.isRequired,
-  }
-
-  render() {
-    const pct = this.props.completePct*100
-    return (
-      <div className="container-fluid p-0">
-        <div className="row">
-          <div className="col">
-            <div className="profile-completion m-b-20">
-              <div className="m-b-10">
-                <span className="font-weight-bold">Complete your ID</span>
+const ProfileCompletion = () => {
+  const pct = this.props.completePct*100
+  return (
+    <div className="container-fluid p-0">
+      <div className="row">
+        <div className="col">
+          <div className="profile-completion m-b-20">
+            <div className="m-b-10">
+              <span className="font-weight-bold">Complete your ID</span>
+            </div>
+            <div className="progress">
+              <div
+                className="progress-bar progress-bar-blue"
+                role="progressbar"
+                style={{width: `${pct}%`}}
+                aria-valuenow={`${pct}`}
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+                <span className={pct === 0 ? 'progress-bar-zero' : ''}>
+                  {`${pct}%`}
+                </span>
               </div>
-              <div className="progress">
-                <div
-                  className="progress-bar progress-bar-blue"
-                  role="progressbar"
-                  style={{width: `${pct}%`}}
-                  aria-valuenow={`${pct}`}
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span className={pct === 0 ? 'progress-bar-zero' : ''}>
-                    {`${pct}%`}
-                  </span>
-                </div>
-              </div>
-              <div className="profile-completion-sub m-t-10">
-                Complete your Blockstack ID by adding your name and a verified social account.
-              </div>
+            </div>
+            <div className="profile-completion-sub m-t-10">
+              Complete your Blockstack ID by adding your name and a verified social account.
             </div>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+ProfileCompletion.propTypes = {
+  completePct: PropTypes.number.isRequired
 }
 
 export default ProfileCompletion
