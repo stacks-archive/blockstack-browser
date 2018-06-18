ea0d4e1ebc8e465b3abb09fc87d115c82a5dc3ad
Matt Davis <matt.davis@mediacurrent.com>
2018-06-18 16:27:10 -0700

    Remove extra space, trigger tests

--- app/js/sign-in/views/_initial.js
+++ app/js/sign-in/views/_initial.js
@@ -48,7 +48,7 @@ const InitialSignInScreen = ({ next, ...rest }) => {
       },
       children: (
         <Type.p>
-          Enter your Magic Recovery Code (we sent it to you when you created your ID) 
+          Enter your Magic Recovery Code (we sent it to you when you created your ID)
           or Secret Recovery Key (those 12 or 24 words you recorded).
         </Type.p>
       )
