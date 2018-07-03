0eda591ae543c77d7a52ce39aafa87c7e325bbd3
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-07-03 15:03:33 -0500

    remove webp script

--- utils/webp.js
+++
@@ -1,8 +0,0 @@
-const imagemin = require('imagemin')
-const imageminWebp = require('imagemin-webp')
-
-imagemin(['build/static/images/**/*.{jpg,png}'], 'build/static/images', {
-  use: [imageminWebp({ quality: 45 })]
-}).then(() => {
-  console.log('Images converted to webp!')
-})
