ad254aa4f48782f37252b90926ca8b7cd7c24351
Craig Ian Smith <mr.craig.ian.smith@gmail.com>
2018-09-03 11:15:36 +0930

    Add extra tests to SearchBar component

--- test/profiles/components/SearchBar.test.js
+++ test/profiles/components/SearchBar.test.js
@@ -4,18 +4,23 @@ import { shallow } from 'enzyme'
 import { SearchBar } from '../../../app/js/profiles/components/SearchBar'
 
 describe('<SearchBar />', () => {
-  let props
   let wrapper
-  const context = { 
-    router: {},
-  }
-  before(() => {
-    props = {
+  const props = {
       query: 'search term',
       placeholder: 'Search...',
       updateQuery: () => {},
-    }
+  }
+  const context = { 
+    router: {
+      push: function(newPath) { 
+        this.newPath = newPath
+        console.log(newPath)
+      },
+      getPath: function() { return this.newPath }
+    },
+  }
 
+  beforeEach(() => {
     wrapper = shallow(<SearchBar {...props} />, { context })
   })
 
@@ -48,5 +53,30 @@ describe('<SearchBar />', () => {
     })
     expect(wrapper.instance().submitQuery.called).to.equal(true)
   })
+
+  it('correctly creates a new path for a blockstackID', () => {
+    let id = 'blockstack.id'
+    makeQuery(id)
+    expect(wrapper.context().router.getPath())
+      .to.equal(`/profiles/${id}`)
+  })
+
+  it('correctly creates a new path for a search query', () => {
+    let query = 'a query'
+    makeQuery(query)
+    expect(wrapper.context().router.getPath())
+      .to.equal(`/profiles/i/search/${query.replace(' ', '%20')}`)
+  })
+
+  function makeQuery(query) {
+    wrapper.find('input').simulate('change', {
+      target: {
+        value: query
+      }
+    }) 
+    wrapper.find('input').simulate('keypress', {
+      key: 'Enter'
+    })
+  }
   
 })
