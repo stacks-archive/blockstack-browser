96f98edaaa00ef361f8167eb45dfc03a5f08298b
Craig Ian Smith <mr.craig.ian.smith@gmail.com>
2018-09-03 13:08:35 +0930

    Add beginning of EditAccount tests

---
+++ test/profiles/components/EditAccount.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import { EditAccount } from '../../../app/js/profiles/components/EditAccount'
+
+describe('<EditAccount />', () => {
+  let wrapper
+
+  it('compiles', () => {
+    expect(true)
+  })
+})  
