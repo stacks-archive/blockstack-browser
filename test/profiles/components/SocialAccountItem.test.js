199d18071ef377b5a5ce73e43ba2278e87842a73
Craig Ian Smith <mr.craig.ian.smith@gmail.com>
2018-09-05 15:40:33 +0930

    Remove skipped test so that it doesn't clutter up the test output

--- test/profiles/components/SocialAccountItem.test.js
+++ test/profiles/components/SocialAccountItem.test.js
@@ -17,7 +17,6 @@ describe('<SocialAccountItem />', () => {
     before(() => {
       props.verified = true
       wrapper = shallow(<SocialAccountItem {...props} />)
-      console.log(wrapper.debug())
     })
 
     it('should render a list item which has verified class', () => {
@@ -26,20 +25,6 @@ describe('<SocialAccountItem />', () => {
 
     it('should render an icon with a check circle', () => {
       expect(wrapper.find('span.status i').hasClass('fa-check-circle')).to.equal(true)
-    })
-
-    // Skip this test, as it requires making the openInNewTab util method
-    // a method of the class under test
-    xit('onVerifiedCheckmarkClick', () => {
-      props.proofUrl = 'theproofURL'
-      const mockEvent = {
-        preventDefault: () => { return true },
-        stopPropagation: () => { return true } 
-      }
-      sinon.stub(wrapper.instance(), 'openInNewTab')
-      wrapper.find('span.status').simulate('click', mockEvent)
-      expect(wrapper.instance().openInNewTab.calledWith(props.proofUrl))
-        .to.equal(true)
     })
 
     it('should render a tooltip which states "Verified"', () => {
