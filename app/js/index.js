374c864d4049060562efabb438b1e69515826a5b
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-09-20 12:40:28 -0500

    don't use console.log for sw messages

--- app/js/index.js
+++ app/js/index.js
@@ -57,9 +57,9 @@ if (process.env.NODE_ENV === 'production') {
         const registration = await navigator.serviceWorker.register(
           '/static/js/sw.js'
         )
-        console.log('SW registered: ', registration)
+        logger.log('SW registered: ', registration)
       } catch (registrationError) {
-        console.log('SW registration failed: ', registrationError)
+        logger.log('SW registration failed: ', registrationError)
       }
     })
   }
