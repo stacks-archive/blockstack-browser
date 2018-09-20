600383d2d8f3dc78d5cc0003cd9a343d9e5ddb86
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-09-20 14:42:08 -0500

    remove global import of bip39 from decrypt worker

--- app/js/utils/workers/decrypt.worker.js
+++ app/js/utils/workers/decrypt.worker.js
@@ -1,55 +1,57 @@
-import bip39 from 'bip39'
 import crypto from 'crypto'
 import triplesec from 'triplesec'
 
-function denormalizeMnemonic(normalizedMnemonic) {
+async function denormalizeMnemonic(normalizedMnemonic) {
+  const bip39 = await import(/* webpackChunkName: 'bip39' */ 'bip39')
   return bip39.entropyToMnemonic(normalizedMnemonic)
 }
 
-function decryptMnemonic(dataBuffer, password) {
-  return Promise.resolve().then(() => {
-    const salt = dataBuffer.slice(0, 16)
-    const hmacSig = dataBuffer.slice(16, 48)   // 32 bytes
-    const cipherText = dataBuffer.slice(48)
-    const hmacPayload = Buffer.concat([salt, cipherText])
+async function decryptMnemonic(dataBuffer, password) {
+  const bip39 = await import(/* webpackChunkName: 'bip39' */ 'bip39')
 
-    const keysAndIV = crypto.pbkdf2Sync(password, salt, 100000, 48, 'sha512')
-    const encKey = keysAndIV.slice(0, 16)
-    const macKey = keysAndIV.slice(16, 32)
-    const iv = keysAndIV.slice(32, 48)
+  const salt = dataBuffer.slice(0, 16)
+  const hmacSig = dataBuffer.slice(16, 48) // 32 bytes
+  const cipherText = dataBuffer.slice(48)
+  const hmacPayload = Buffer.concat([salt, cipherText])
 
-    const decipher = crypto.createDecipheriv('aes-128-cbc', encKey, iv)
-    let plaintext = decipher.update(cipherText, '', 'hex')
-    plaintext += decipher.final('hex')
+  const keysAndIV = crypto.pbkdf2Sync(password, salt, 100000, 48, 'sha512')
+  const encKey = keysAndIV.slice(0, 16)
+  const macKey = keysAndIV.slice(16, 32)
+  const iv = keysAndIV.slice(32, 48)
 
-    const hmac = crypto.createHmac('sha256', macKey)
-    hmac.write(hmacPayload)
-    const hmacDigest = hmac.digest()
+  const decipher = crypto.createDecipheriv('aes-128-cbc', encKey, iv)
+  let plaintext = decipher.update(cipherText, '', 'hex')
+  plaintext += decipher.final('hex')
 
-    // hash both hmacSig and hmacDigest so string comparison time
-    // is uncorrelated to the ciphertext
-    const hmacSigHash = crypto.createHash('sha256')
-      .update(hmacSig)
-      .digest()
-      .toString('hex')
+  const hmac = crypto.createHmac('sha256', macKey)
+  hmac.write(hmacPayload)
+  const hmacDigest = hmac.digest()
 
-    const hmacDigestHash = crypto.createHash('sha256')
-      .update(hmacDigest)
-      .digest()
-      .toString('hex')
+  // hash both hmacSig and hmacDigest so string comparison time
+  // is uncorrelated to the ciphertext
+  const hmacSigHash = crypto
+    .createHash('sha256')
+    .update(hmacSig)
+    .digest()
+    .toString('hex')
 
-    if (hmacSigHash !== hmacDigestHash) {
-      // not authentic
-      throw new Error('Wrong password (HMAC mismatch)')
-    }
+  const hmacDigestHash = crypto
+    .createHash('sha256')
+    .update(hmacDigest)
+    .digest()
+    .toString('hex')
 
-    const mnemonic = denormalizeMnemonic(plaintext)
-    if (!bip39.validateMnemonic(mnemonic)) {
-      throw new Error('Wrong password (invalid plaintext)')
-    }
+  if (hmacSigHash !== hmacDigestHash) {
+    // not authentic
+    throw new Error('Wrong password (HMAC mismatch)')
+  }
 
-    return mnemonic
-  })
+  const mnemonic = await denormalizeMnemonic(plaintext)
+  if (!bip39.validateMnemonic(mnemonic)) {
+    throw new Error('Wrong password (invalid plaintext)')
+  }
+
+  return mnemonic
 }
 
 function decryptLegacy(dataBuffer, password) {
@@ -76,7 +78,7 @@ export async function decrypt(hexEncryptedKey, password) {
 
   try {
     mnemonic = await decryptMnemonic(dataBuffer, password)
-  } catch(err) {
+  } catch (err) {
     mnemonic = await decryptLegacy(dataBuffer, password)
   }
 
