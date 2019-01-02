32d5d3251c6558cb2ac6f47c0f6209e395a06d14
Ken <yukanliao@gmail.com>
2019-01-02 15:19:02 -0500

    Fix flow error.

--- app/js/update/index.js
+++ app/js/update/index.js
@@ -361,6 +361,7 @@ UpdatePage.propTypes = {
   router: PropTypes.object,
   identityAddresses: PropTypes.array,
   createNewIdentityWithOwnerAddress: PropTypes.func.isRequired,
+  refreshIdentities: PropTypes.func.isRequired,
   setDefaultIdentity: PropTypes.func.isRequired,
   initializeWallet: PropTypes.func.isRequired,
   updateApi: PropTypes.func.isRequired,
