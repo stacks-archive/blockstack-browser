3cfb20426ea52997a88f978b78e35786d54683d7
Matthew Little <zone117x@gmail.com>
2019-01-04 15:24:47 -0500

    Add protocol echo reply code for use with blockstack.js new protocol detection feature

--- app/js/auth/index.js
+++ app/js/auth/index.js
@@ -7,12 +7,14 @@ import { connect } from 'react-redux'
 import { AuthActions } from './store/auth'
 import { decodeToken } from 'jsontokens'
 import { parseZoneFile } from 'zone-file'
+import queryString from 'query-string'
 import {
   makeAuthResponse,
   getAuthRequestFromURL,
   redirectUserToApp,
   getAppBucketUrl,
-  isLaterVersion
+  isLaterVersion,
+  updateQueryStringParameter
 } from 'blockstack'
 import { AppsNode } from '@utils/account-utils'
 import {
@@ -115,6 +117,7 @@ class AuthPage extends React.Component {
     this.state = {
       currentIdentityIndex: this.props.defaultIdentity,
       authRequest: null,
+      echoRequestId: null,
       appManifest: null,
       coreSessionToken: null,
       decodedToken: null,
@@ -137,12 +140,16 @@ class AuthPage extends React.Component {
   }
 
   componentWillMount() {
+    const queryDict = queryString.parse(location.search)
+    const echoRequestId = queryDict.echo
+
     const authRequest = getAuthRequestFromURL()
     const decodedToken = decodeToken(authRequest)
     const { scopes } = decodedToken.payload
 
     this.setState({
       authRequest,
+      echoRequestId,
       decodedToken,
       scopes: {
         ...this.state.scopes,
@@ -154,8 +161,30 @@ class AuthPage extends React.Component {
     this.props.verifyAuthRequestAndLoadManifest(authRequest)
   }
 
+  redirectUserToEchoReply() {
+    let redirectURI = this.state.decodedToken.payload.redirect_uri
+    if (redirectURI) {
+      // Get the current localhost authentication url that the app will redirect back to,
+      // and remove the 'echo' param from it.
+      const authContinuationURI = updateQueryStringParameter(window.location.href, 'echo', '')
+      redirectURI = updateQueryStringParameter(redirectURI, 'echoReply', this.state.echoRequestId)
+      redirectURI = updateQueryStringParameter(redirectURI, 'authContinuation', encodeURIComponent(authContinuationURI))
+    } else {
+      throw new Error('Invalid redirect echo reply URI')
+    }
+    this.setState({ responseSent: true })
+    window.location = redirectURI
+  }
+  
   componentWillReceiveProps(nextProps) {
+
     if (!this.state.responseSent) {
+
+      if (this.state.echoRequestId) {
+        this.redirectUserToEchoReply()
+        return
+      }  
+
       const storageConnected = this.props.api.storageConnected
       this.setState({
         storageConnected
