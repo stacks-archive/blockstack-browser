8151e897d6e6f4781e8f43c89828352aa8637c04
zone117x <zone117x@gmail.com>
2019-01-14 16:28:36 -0500

    Windows support for ctrl+click "Enable debug mode" option.

--- native/windows/ProtocolHandler/Program.cs
+++ native/windows/ProtocolHandler/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
 using System.Windows.Forms;
 
 namespace ProtocolHandler
@@ -7,13 +9,36 @@ namespace ProtocolHandler
     public class Program
     {
         static String BLOCKSTACK_PROTOCOL_PART = "blockstack:";
+        const string BLOCKSTACK_PROTOCOL_HANDLER_PIPE = "BLOCKSTACK_PROTOCOL_HANDLER_PIPE";
+
         static void Main(String[] args)
         {
             if (args.Length < 1 || args[0].Length < BLOCKSTACK_PROTOCOL_PART.Length)
                 return;
-            String authPart = args[0].Substring(BLOCKSTACK_PROTOCOL_PART.Length);
-            String urlOpen = "http://localhost:8888/auth?authRequest=" + authPart;
-            Process.Start(urlOpen);
+
+            try
+            {
+                using (var pipeClient = new NamedPipeClientStream(".", BLOCKSTACK_PROTOCOL_HANDLER_PIPE, PipeDirection.Out))
+                {
+                    // Connect to the main Blockstack process.
+                    pipeClient.Connect(1500);
+                    using (var sr = new StreamWriter(pipeClient, Encoding.UTF8))
+                    {
+                        sr.Write(args[0]);
+                    }
+                }
+            }
+            catch
+            {
+                // TODO: This used to launch the system web browser to http://localhost:8888 
+                //       which would simply fail to load if the Blockstack app was not running.
+                //       Now that we can detect when its not running, should be start the process
+                //       for the user?
+                MessageBox.Show("Blockstack does not appear to be running.");
+                throw;
+            }
+
         }
+
     }
 }
