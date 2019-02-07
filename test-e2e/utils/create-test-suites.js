7d07b0ecfc1470757aaa572869b358e2ae8c3b86
Matthew Little <zone117x@gmail.com>
2019-02-07 14:49:39 -0500

    Support for running tests on BrowserStack with locally hosted auth-browser.

--- test-e2e/utils/create-test-suites.js
+++ test-e2e/utils/create-test-suites.js
@@ -4,11 +4,166 @@ const { expect } = require('chai');
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
+const url = require('url');
+const browserStackLocal = require('browserstack-local').Local;
 const ExtendedWebDriver = require('./ExtendedWebDriver');
-const getTestEnvironments = require('./get-test-environments');
+const browserStackEnvironments = require('./browserstack-environments');
 const helpers = require('./helpers');
 
 // selenium-webdriver docs: https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/lib/webdriver.html
+
+const BROWSERSTACK_HUB_URL = 'http://hub-cloud.browserstack.com/wd/hub';
+
+
+// Determine which browser host endpoint to run tests against.
+const browserHostUrl = (() => {
+  const E2E_BROWSER_HOST = 'E2E_BROWSER_HOST';
+  const PROD_HOST = 'https://browser.blockstack.org';
+  const hostUrl = process.env[E2E_BROWSER_HOST] || PROD_HOST;
+  if (!process.env[E2E_BROWSER_HOST]) {
+    console.warn(`Warning: The browser host url was not set via the ${E2E_BROWSER_HOST} env var.. running tests against the production endpoint "${PROD_HOST}"`);
+  } else {
+    console.log(`Running e2e tests against endpoint ${hostUrl}`);
+  }
+  return hostUrl;
+})();
+
+/**
+ * Checks environment vars for BrowserStack usage settings.
+ */
+const browserStackConfig = (() => {
+  const USE_BROWSERSTACK = 'USE_BROWSERSTACK';
+  const BROWSERSTACK_AUTH = 'BROWSERSTACK_AUTH';
+  const config = { 
+    enabled: process.env[USE_BROWSERSTACK] && process.env[USE_BROWSERSTACK] !== 'false', 
+    user: '', key: ''
+  };
+  if (config.enabled) {
+    const browserstackAuth = process.env[BROWSERSTACK_AUTH];
+    if (!browserstackAuth) {
+      const errMsg = `The BrowserStack auth must be set as environment variables. Use the format \`${BROWSERSTACK_AUTH}="user:key"\``;
+      console.error(errMsg);
+      throw new Error(errMsg);
+    }
+    // Auth string formatted as "user:key"
+    [config.user, config.key] = browserstackAuth.trim().split(/:(.+)/);
+  }
+  return config;
+})();
+
+/**
+ * True if the auth-browser host endpoint is set to localhost and BrowserStack testing is enabled.
+ * @see {@link https://www.npmjs.com/package/browserstack-local }
+ * @see {@link https://www.browserstack.com/local-testing }
+ */
+const browserStackLocalEnabled = (() => {
+  const isLocalhost = ['localhost', '127.0.0.1'].includes(url.parse(browserHostUrl).hostname);
+  return isLocalhost && browserStackConfig.enabled;
+})();
+
+
+let blockStackLocalInstance;
+before(async () => {
+  // Check if BrowserStackLocal needs to be initialized before running tests..
+  if (browserStackLocalEnabled) {
+    console.log(`BrowserStack is enabled the test endpoint is localhost, setting up BrowserStack Local..`);
+    blockStackLocalInstance = new browserStackLocal();
+    return await new Promise((resolve, reject) => {
+      blockStackLocalInstance.start({ key: browserStackConfig.key, force: 'true' }, (error) => {
+        if (error) {
+          console.error(`Error starting BrowserStack Local: ${error}`);
+          reject(error)
+        } else {
+          console.log(`BrowserStack Local started`);
+          resolve(browserHostUrl);
+        }
+      });
+    });
+  }
+});
+after(() => {
+  // Check if BrowserStackLocal needs to be disposed off after running tests..
+  if (blockStackLocalInstance && blockStackLocalInstance.isRunning()) {
+    return new Promise((resolve, reject) => {
+      blockStackLocalInstance.stop((error) => {
+        if (error) {
+          console.error(`Error stopping BrowserStack Local: ${error}`);
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+});
+
+
+/**
+ * @typedef {Object} TestEnvironment
+ * @property {string} description Human-readable name of the operating system & web browser.
+ * @property {Promise<ExtendedWebDriver>} createDriver Promise that resolves to a ready-to-use WebDriver instance.
+ */
+
+/**
+ * @generator
+ * @param {string} user BrowserStack user credential.
+ * @param {string} key BrowserStack key credential.
+ * @yields {TestEnvironment}
+ */
+function* getBrowserstackEnvironments(user, key) {
+  for (let capability of browserStackEnvironments) {
+    capability = Object.assign(capability, {
+      'browserstack.user': user,
+      'browserstack.key': key
+    });
+    if (browserStackLocalEnabled) {
+      capability['browserstack.local'] = 'true';
+    }
+    yield {
+      description: capability.desc,
+      createDriver: async () => {
+        const driver = await new Builder().
+          usingServer(BROWSERSTACK_HUB_URL).
+          withCapabilities(capability).
+          build();
+        await driver.manage().setTimeouts({ implicit: 1000, pageLoad: 10000 });
+        return new ExtendedWebDriver(driver);
+      }
+    };
+  }
+}
+
+/**
+ * Generates test environments for the local machine. Always includes 'chrome' and 'firefox'.
+ * If on macOS then also includes 'safari'. If on Windows then also includes 'edge'. 
+ * @generator
+ * @yields {TestEnvironment}
+ */
+function* getLocalSystemBrowserEnvironments() {
+  const browsers = ['firefox', 'chrome'];
+
+  // Ensure the browser webdriver binaries are added to env path
+  require('chromedriver');
+  require('geckodriver');
+
+  if (process.platform === 'darwin') {
+    browsers.push('safari');
+  } else if (process.platform === 'win32') {
+    browsers.push('edge');
+  }
+  for (let browser of new Set(browsers)) {
+    yield {
+      description: `${process.platform} ${browser}`,
+      createDriver: async () => {
+        const driver = await new Builder()
+          .forBrowser(browser)
+          .build();
+        await driver.manage().setTimeouts({ implicit: 1000, pageLoad: 10000 });
+        return new ExtendedWebDriver(driver);
+      }
+    };
+  }
+}
 
 /**
  * @typedef {Object} TestInputs
@@ -33,14 +188,18 @@ const helpers = require('./helpers');
  */
 function createTestSuites(title, defineTests) {
 
-  for (const testEnvironment of getTestEnvironments()) {
+  const testEnvironments = browserStackConfig.enabled 
+    ? getBrowserstackEnvironments(browserStackConfig.user, browserStackConfig.key)
+    : getLocalSystemBrowserEnvironments();
+
+  for (const testEnvironment of testEnvironments) {
 
     describe(`${title} [${testEnvironment.description}]`, () => {
 
       /** @type {TestInputs} */
       const testInputs = {
         envDesc: testEnvironment.description,
-        browserHostUrl: process.env['E2E_BROWSER_HOST'] || 'https://browser.blockstack.org',
+        browserHostUrl: browserHostUrl,
         driver: {}
       };
 
