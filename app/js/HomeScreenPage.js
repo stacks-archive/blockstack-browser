bd201655f8ff1f9079b536c773706c1aa050411f
Hank Stoever <hstove@gmail.com>
2019-03-08 16:15:23 -0800

    hotfix: fixes error on update page when state update needed

--- app/js/HomeScreenPage.js
+++ app/js/HomeScreenPage.js
@@ -29,17 +29,22 @@ const Loading = ({ ...rest }) => (
   </Flex>
 )
 
-const Content = ({ topApps, allApps, ...rest }) => (
-  <Box maxWidth={1280} width="100%" mx="auto" p={[1, 2, 4]} {...rest}>
-    <AppsSection title="Top Apps" apps={topApps} limit={24} />
-    {allApps
-      .sort((a, b) => a.label.localeCompare(b.label))
-      .map(category => {
-        const apps = category.apps.sort((a, b) => a.name.localeCompare(b.name))
-        return <AppsSection title={category.label} apps={apps} />
-      })}
-  </Box>
-)
+const Content = ({ topApps, allApps, ...rest }) => {
+  if (!allApps) {
+    return null
+  }
+  return (
+    <Box maxWidth={1280} width="100%" mx="auto" p={[1, 2, 4]} {...rest}>
+      <AppsSection title="Top Apps" apps={topApps} limit={24} />
+      {allApps
+        .sort((a, b) => a.label.localeCompare(b.label))
+        .map(category => {
+          const apps = category.apps.sort((a, b) => a.name.localeCompare(b.name))
+          return <AppsSection title={category.label} apps={apps} />
+        })}
+    </Box>
+  )
+}
 
 const AppsSection = ({ title, apps, limit, category, ...rest }) => {
   let appsList = apps.filter(app => app.imgixImageUrl)
