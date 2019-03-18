26dd35ccea21923b0a6618f7e7517c5a930b316d
Ken <yukanliao@gmail.com>
2019-03-18 11:57:58 -0400

    Tweak onboarding success page.

--- app/js/sign-up/views/_success.js
+++ app/js/sign-up/views/_success.js
@@ -7,6 +7,7 @@ import React from 'react'
 import { ShellScreen, Type } from '@blockstack/ui'
 import PropTypes from 'prop-types'
 import { ProfileScreen } from '@components/ui/containers/profile'
+import { Flex } from '@components/ui/components/primitives'
 
 const Success = ({
   finish,
@@ -26,30 +27,50 @@ const Success = ({
     : null
 
   const props = {
+    title: {
+      children: (
+        <ProfileScreen 
+          user={user} 
+          justifyContent="center"
+          alignItems="center"
+          style={{
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+        </ProfileScreen>
+      )
+    },
     content: {
       grow: 1,
+      form: {
+        fields: [],
+        actions: {
+          items: [
+            {
+              label: (
+                <>
+                  Go to {app && app.name ? app.name : 'Blockstack'}
+                </>
+              ),
+              primary: true,
+              onClick: () => finish()
+            }
+          ]
+        }
+      },
       children: (
-        <ProfileScreen user={user}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+        >
           <Type.p>
             Your ID is ready and we sent recovery instructions to your email.
             You can also view your{' '}
             <Type.a onClick={() => goToRecovery()}>Secret Recovery Key</Type.a>.
           </Type.p>
-        </ProfileScreen>
+        </Flex>
       )
-    },
-    actions: {
-      items: [
-        {
-          label: (
-            <>
-              Go to {app && app.name ? app.name : 'Blockstack'}
-            </>
-          ),
-          primary: true,
-          onClick: () => finish()
-        }
-      ]
     }
   }
   return <ShellScreen {...rest} {...props} />
