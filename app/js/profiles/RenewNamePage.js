97c867976c0e665c414402e1c66e71916ead3540
Ken <yukanliao@gmail.com>
2019-07-01 23:46:40 -0400

    Fix lint.

--- app/js/profiles/RenewNamePage.js
+++ app/js/profiles/RenewNamePage.js
@@ -2,16 +2,11 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { Link } from 'react-router'
 import { AccountActions } from '../account/store/account'
 import { IdentityActions } from './store/identity'
 import { RegistrationActions } from './store/registration'
-import { findAddressIndex, decryptBitcoinPrivateKey } from '@utils'
+import { decryptBitcoinPrivateKey } from '@utils'
 import InputGroup from '@components/InputGroup'
-
-import log4js from 'log4js'
-
-const logger = log4js.getLogger(__filename)
 
 function mapStateToProps(state) {
   return {
@@ -20,7 +15,7 @@ function mapStateToProps(state) {
     encryptedBackupPhrase: state.account.encryptedBackupPhrase,
     identityKeypairs: state.account.identityAccount.keypairs,
     localIdentities: state.profiles.identity.localIdentities,
-    currentIdentity: state.profiles.identity.current,
+    currentIdentity: state.profiles.identity.current
   }
 }
 
@@ -39,7 +34,10 @@ class RenewNamePage extends Component {
     currentIdentity: PropTypes.object.isRequired,
     identityAddresses: PropTypes.array.isRequired,
     identityKeypairs: PropTypes.array.isRequired,
-    localIdentities: PropTypes.object.isRequired
+    localIdentities: PropTypes.object.isRequired,
+    routeParams: PropTypes.object.isRequired,
+    encryptedBackupPhrase: PropTypes.string.isRequired,
+    renewDomain: PropTypes.function.isRequired
   }
 
   constructor(props) {
@@ -47,7 +45,7 @@ class RenewNamePage extends Component {
     this.state = {
       error: null,
       success: null,
-      renewInProgress: false,
+      renewInProgress: false
     }
     this.onValueChange = this.onValueChange.bind(this)
     this.renew = this.renew.bind(this)
@@ -112,40 +110,38 @@ class RenewNamePage extends Component {
 
     return (
       <div className="container-fluid p-0">
-
-          <div className="row">
-            <div className="col-md-12">
-              <h3>Enter your password to renew {currentIdentity.username}</h3>
-            </div>
-            <div className="col-md-12">
-              <form onSubmit={this.renew}>
-                <InputGroup
-                  data={this.state}
-                  onChange={this.onValueChange}
-                  name="password"
-                  label=""
-                  placeholder="Password"
-                  type="password"
-                  required
-                />
-                {this.state.error ? <div className="alert alert-danger">{this.state.error}</div> : null}
-                {this.state.success ? <div className="alert alert-success">{this.state.success}</div> : null}
-                <button
-                  type="submit"
-                  onClick={this.renew}
-                  className="btn btn-primary btn-block"
-                  disabled={renewInProgress}
-                >
-                  {renewInProgress ? (
-                    <span>Generating renew transaction...</span>
-                  ) : (
-                    <span>Renew</span>
-                  )}
-                </button>
-              </form>
-            </div>
+        <div className="row">
+          <div className="col-md-12">
+            <h3>Enter your password to renew {currentIdentity.username}</h3>
           </div>
-
+          <div className="col-md-12">
+            <form onSubmit={this.renew}>
+              <InputGroup
+                data={this.state}
+                onChange={this.onValueChange}
+                name="password"
+                label=""
+                placeholder="Password"
+                type="password"
+                required
+              />
+              {this.state.error ? <div className="alert alert-danger">{this.state.error}</div> : null}
+              {this.state.success ? <div className="alert alert-success">{this.state.success}</div> : null}
+              <button
+                type="submit"
+                onClick={this.renew}
+                className="btn btn-primary btn-block"
+                disabled={renewInProgress}
+              >
+                {renewInProgress ? (
+                  <span>Generating renew transaction...</span>
+                ) : (
+                  <span>Renew</span>
+                )}
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     )
   }
