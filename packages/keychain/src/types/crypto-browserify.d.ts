03cae3b2a0e617a6a19db6436871de93ee120668
Hank Stoever <hstove@gmail.com>
2019-08-30 12:01:11 -0700

    module declaration for createDecipheriv

--- packages/keychain/src/types/crypto-browserify.d.ts
+++ packages/keychain/src/types/crypto-browserify.d.ts
@@ -1,6 +1,7 @@
 interface Hash {
   update(data: string | Buffer): Hash
   digest(encoding: string): string
+  digest(): Buffer
 }
 
 interface Cipher {
@@ -18,6 +19,7 @@ declare module 'crypto-browserify' {
   export function randomBytes(size: number): Buffer
   export function pbkdf2Sync(password: string, salt: Buffer, iterations: number, keylen: number, digest: string): Buffer
   export function createCipheriv(algorithm: string, key: string | Buffer, iv: string | Buffer): Cipher
+  export function createDecipheriv(algorithm: string, key: string | Buffer, iv: string | Buffer): Cipher
   export function createHmac(algorithm: string, key: string | Buffer): Hmac
   export function createHash(algorithm: string): Hash
 }
