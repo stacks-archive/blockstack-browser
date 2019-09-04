293e890a78786a94942bd1888c5050d8afb9ebea
Hank Stoever <hstove@gmail.com>
2019-09-04 11:02:46 -0700

    remove the background app from the extension

--- packages/app/src/ts/contentScripts/counter/index.tsx
+++ packages/app/src/ts/contentScripts/counter/index.tsx
@@ -1,21 +1,23 @@
-import * as React from 'react';
-import { Store } from 'webext-redux';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import CounterApp from './containers/CounterApp';
+// import React from 'react'
+// import { Store } from 'webext-redux'
+// import ReactDOM from 'react-dom'
+// import { Provider } from 'react-redux'
+// import CounterApp from './containers/CounterApp'
 
-import { createDomAnchor } from '../../scripts/dom';
+// import { createDomAnchor } from '../../scripts/dom'
 
-createDomAnchor('counter-root');
+// createDomAnchor('counter-root')
 
-const store = new Store({
-	portName: 'ExPort' // Communication port between the background component and views such as browser tabs.
-});
+// const store = new Store({
+//   portName: 'ExPort' // Communication port between the background component and views such as browser tabs.
+// })
 
-store.ready().then(() => {
-	ReactDOM.render(
-		<Provider store={store as any}>
-			<CounterApp />
-		</Provider>
-		, document.getElementById('counter-root'));
-});
+// // eslint-disable-next-line @typescript-eslint/no-floating-promises
+// store.ready().then(() => {
+//   ReactDOM.render(
+//     <Provider store={store as any}>
+//       <CounterApp />
+//     </Provider>
+//     , document.getElementById('counter-root'))
+// })
+console.log('Running in the background!')
