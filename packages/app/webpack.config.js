0ec9e49f9b1a253f6d89b8341a5c4391133e149b
Hank Stoever <hstove@gmail.com>
2019-11-03 21:07:07 -0800

    rename content script, remove unused utils

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -10,21 +10,20 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const locateContentScripts = require('./utils/locateContentScripts')
 /* eslint-enable @typescript-eslint/no-var-requires */
 
-const sourceRootPath = path.join(__dirname, 'src')
-const contentScriptsPath = path.join(sourceRootPath, 'ts', 'contentScripts')
+const sourceRootPath = path.join(__dirname, 'src', 'ts')
 const distRootPath = path.join(__dirname, 'dist')
 const nodeEnv = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
 const webBrowser = process.env.WEB_BROWSER ? process.env.WEB_BROWSER : 'chrome'
 
 module.exports = {
   entry: {
-    background: path.join(sourceRootPath, 'ts', 'background', 'index.ts'),
-    options: path.join(sourceRootPath, 'ts', 'options', 'index.tsx'),
-    popup: path.join(sourceRootPath, 'ts', 'popup', 'index.tsx'),
-    worker: path.join(sourceRootPath, 'ts', 'worker', 'index.ts'),
-    inpage: path.join(sourceRootPath, 'ts', 'inpage', 'index.ts'),
-    actions: path.join(sourceRootPath, 'ts', 'actions', 'index.tsx'),
-    ...locateContentScripts(contentScriptsPath)
+    background: path.join(sourceRootPath, 'background', 'index.ts'),
+    options: path.join(sourceRootPath, 'options', 'index.tsx'),
+    popup: path.join(sourceRootPath, 'popup', 'index.tsx'),
+    worker: path.join(sourceRootPath, 'worker', 'index.ts'),
+    inpage: path.join(sourceRootPath, 'inpage', 'index.ts'),
+    actions: path.join(sourceRootPath, 'actions', 'index.tsx'),
+    "message-bus": path.join(sourceRootPath, 'content-scripts', 'message-bus.ts'),
   },
   output: {
     path: distRootPath,
@@ -130,7 +129,7 @@ if (process.env.EXT_ENV === 'watch') {
         background: 'background',
         options: 'index',
         popup: 'popup',
-        contentScript: ['counter']
+        contentScript: ['message-bus']
       }
     })
   )
