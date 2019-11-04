a75f4c985b152929fda335eb571ac943cee994eb
Hank Stoever <hstove@gmail.com>
2019-11-03 21:13:11 -0800

    remove 'scripts' from starter kit

--- packages/app/src/ts/scripts/dom.ts
+++
@@ -1,5 +0,0 @@
-export const createDomAnchor = (anchorId: string) => {
-  const anchor = document.createElement('div')
-  anchor.id = anchorId
-  document.body.insertBefore(anchor, document.body.childNodes[0])
-}
