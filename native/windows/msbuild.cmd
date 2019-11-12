4e52b563bb6483e38bb552623d936171c268ed99
Matt Little <zone117x@gmail.com>
2019-11-12 16:13:49 -0700

    Fix building & signing msi on Windows with Visual Studio 2019

--- native/windows/msbuild.cmd
+++ native/windows/msbuild.cmd
@@ -1,10 +1,10 @@
 @if not defined _echo echo off
+setlocal enabledelayedexpansion
 
 REM Finds and invokes msbuild.exe with the arguments passed to this script.
-REM Requires Visual Studio 2017 to be installed.
+REM Requires Visual Studio to be installed.
 REM See https://github.com/Microsoft/vswhere for details.
 
-set msbuild_dir="MSBuild\15.0\Bin\MSBuild.exe"
 set vswhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 
 if not exist %vswhere% (
@@ -12,12 +12,14 @@ if not exist %vswhere% (
   exit /b 2
 )
 
-for /f "usebackq delims=" %%i in (`%vswhere% -latest -property installationPath`) do (
-  if exist "%%i\%msbuild_dir%" (
-    "%%i\%msbuild_dir%" %*
-    exit /b
-  ) else if not exist "%%i\%msbuild_dir%" (
-    echo "MSBuild.exe not found"
-    exit /b 2
-  )
+for /f "usebackq delims=" %%i in (`%vswhere% -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+  echo Using msbuild: %%i
+  set msbuild_path=%%i
+  goto found_msbuild
 )
+echo MSBuild.exe not found
+exit /b 2
+
+:found_msbuild
+"%msbuild_path%" %*
+exit /b !errorlevel!
