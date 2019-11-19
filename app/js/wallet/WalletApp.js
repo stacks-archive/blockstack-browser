3faf9845c6c4cdf8295278d416e3c0d5c771a3d2
Hank Stoever <hstove@gmail.com>
2019-11-19 15:28:41 -0800

    hotfix to fix #1980

--- app/js/wallet/WalletApp.js
+++ app/js/wallet/WalletApp.js
@@ -32,28 +32,26 @@ class WalletApp extends Component {
     const activeTabUrl = `/wallet/${childPath}`
 
     return (
-      <div className="container-fluid">
-        <div>
-          <Navbar activeTab="wallet" />
-          <div className="container-fluid col-centered form-container-secondary">
-            <strong>NOTE:</strong> You cannot use this wallet to send and receive Stacks (STX) tokens. Also, you cannot use the Bitcoin (BTC) address on this page to fund STX transactions. This wallet and its address <strong>only</strong> support the purchase of Blockstack identities (IDs). <strong>To create or fund STX transactions, use the Stacks Wallet software.</strong> See <a href="https://docs.blockstack.org/org/wallet-install.html" target="_blank">the Stacks Wallet software documentation</a> for more information. 
+      <div>
+        <Navbar activeTab="wallet" />
+        <div className="container-fluid col-centered form-container-secondary">
+          <strong>NOTE:</strong> You cannot use this wallet to send and receive Stacks (STX) tokens. Also, you cannot use the Bitcoin (BTC) address on this page to fund STX transactions. This wallet and its address <strong>only</strong> support the purchase of Blockstack identities (IDs). <strong>To create or fund STX transactions, use the Stacks Wallet software.</strong> See <a href="https://docs.blockstack.org/org/wallet-install.html" target="_blank">the Stacks Wallet software documentation</a> for more information. 
+        </div>
+        <SecondaryNavBar
+          leftButtonTitle="Receive"
+          leftButtonLink="/wallet/receive"
+          isLeftActive={(activeTabUrl === '/wallet/receive')}
+          rightButtonTitle="Send"
+          rightButtonLink="/wallet/send"
+          isRightActive={(activeTabUrl === '/wallet/send')}
+          activeClass="active-wallet"
+          customButtonClass="btn-wallet"
+        />
+        <div className="container-fluid col-centered form-container-secondary">
+          <div>
+            {this.props.children}
           </div>
-          <SecondaryNavBar
-            leftButtonTitle="Receive"
-            leftButtonLink="/wallet/receive"
-            isLeftActive={(activeTabUrl === '/wallet/receive')}
-            rightButtonTitle="Send"
-            rightButtonLink="/wallet/send"
-            isRightActive={(activeTabUrl === '/wallet/send')}
-            activeClass="active-wallet"
-            customButtonClass="btn-wallet"
-          />
-          <div className="container-fluid col-centered form-container-secondary">
-            <div>
-              {this.props.children}
-            </div>
-          </div>
-        </div>  
+        </div>
       </div>
     )
   }
