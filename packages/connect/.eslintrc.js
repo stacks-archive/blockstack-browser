f26287e207651e7ad471784a6253885d1f8de498
Hank Stoever <hstove@gmail.com>
2019-12-03 15:53:58 -0800

    eslint setup

---
+++ packages/connect/.eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    // 'plugin:jsx-a11y/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: '2018',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    es6: true
+  },
+  rules: {
+    quotes: [2, 'single', { avoidEscape: true }],
+    'eol-last': 2,
+    'no-mixed-requires': 0,
+    'no-var': 2,
+    'no-param-reassign': 'error',
+    'brace-style': 2,
+    'no-empty': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false
+        }
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/array-type': 'error',
+    // '@typescript-eslint/semi': ['error', 'never'],
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/promise-function-async': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/class-name-casing': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+  }
+}
