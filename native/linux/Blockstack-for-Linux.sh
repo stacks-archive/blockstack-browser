315f57b28216241797e03bd80f188e8085c3f3b7
Hank Stoever <hstove@gmail.com>
2019-12-17 15:54:57 -0800

    beta version bump

--- native/linux/Blockstack-for-Linux.sh
+++ native/linux/Blockstack-for-Linux.sh
@@ -2,7 +2,7 @@
 
 # This script provides a simple interface for folks to use the docker install
 
-TAG=v0.37.0-beta.6
+TAG=v0.37.0-beta.7
 if [ "$BLOCKSTACK_TAG" ]; then
    TAG="$BLOCKSTACK_TAG"
 fi
