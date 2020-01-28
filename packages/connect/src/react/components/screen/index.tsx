7a1d8bced3b908cf6264a84d9ae364aa17cae2b3
Hank Stoever <hstove@gmail.com>
2020-01-27 16:55:57 -0800

    Merge pull request #20 from blockstack/feature/add-screen-header
    Add in screen-header component

--- packages/connect/src/react/components/screen/index.tsx
+++ packages/connect/src/react/components/screen/index.tsx
@@ -2,4 +2,5 @@ export * from './screen';
 export * from './screen-actions';
 export * from './screen-body';
 export * from './screen-footer';
+export * from './screen-header';
 export * from './screen-loader';
---
+++ packages/connect/src/react/components/screen/screen-header.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Box, Flex, Text } from '@blockstack/ui';
+import ChevronRightIcon from 'mdi-react/ChevronRightIcon';
+import { useAppDetails } from '../../hooks/useAppDetails';
+import { Logo } from '../logo';
+import { AppIcon } from '../app-icon';
+
+interface HeaderTitleProps {
+  title?: string | JSX.Element;
+  hideIcon?: boolean;
+}
+
+const HeaderTitle: React.FC<HeaderTitleProps> = ({ hideIcon, title }) => (
+  <Flex align="center">
+    {hideIcon ? null : <Logo mr={2} />}
+    <Text fontWeight="bold" fontSize={'12px'}>
+      {title}
+    </Text>
+  </Flex>
+);
+
+export interface ScreenHeaderProps {
+  appDetails?: {
+    name: string;
+    icon: string;
+  };
+  title?: string | JSX.Element;
+  close?: () => void;
+  hideIcon?: boolean;
+}
+
+export const ScreenHeader = ({ appDetails, title = 'Data Vault', hideIcon = false, ...rest }: ScreenHeaderProps) => {
+  const { name, icon } = useAppDetails();
+
+  let appName = name;
+  let appIcon = icon;
+
+  if (appDetails) {
+    appName = appDetails.name;
+    appIcon = appDetails.icon;
+  }
+
+  return (
+    <Flex
+      p={[4, 5]}
+      mb={6}
+      borderBottom="1px solid"
+      borderBottomColor="inherit"
+      borderRadius={['unset', '6px 6px 0 0']}
+      bg="white"
+      align="center"
+      justify="space-between"
+      {...rest}
+    >
+      <Flex align="center">
+        {!hideIcon ? <AppIcon src={appIcon} alt={appName || 'loading'} /> : null}
+        {!hideIcon ? (
+          <Box pr={1} pl={2} color="ink.300">
+            <ChevronRightIcon size={20} />
+          </Box>
+        ) : null}
+        <HeaderTitle hideIcon={hideIcon} title={title} />
+      </Flex>
+    </Flex>
+  );
+};
