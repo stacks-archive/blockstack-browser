68d44cdeaa812085a4018ded47f6dd4310b56e57
kyranjamie <kyranjamie@users.noreply.github.com>
2020-01-28 16:30:41 +0100

    fix: Allow optional title (#21)

--- packages/connect/src/react/components/screen/screen-body.tsx
+++ packages/connect/src/react/components/screen/screen-body.tsx
@@ -4,7 +4,7 @@ import { Stack, BoxProps } from '@blockstack/ui';
 import { Title, Body, Pretitle } from '../typography';
 
 export interface ScreenBodyProps extends BoxProps {
-  title: string;
+  title?: string;
   pretitle?: string | React.ElementType;
   body?: (string | JSX.Element)[];
   fullWidth?: boolean;
@@ -13,7 +13,7 @@ export interface ScreenBodyProps extends BoxProps {
 export const ScreenBody = ({ title, body, pretitle, fullWidth, ...rest }: ScreenBodyProps) => (
   <Stack spacing={2} mx={fullWidth ? 0 : 6} {...rest}>
     {pretitle && <Pretitle>{pretitle}</Pretitle>}
-    <Title>{title}</Title>
+    {title && <Title>{title}</Title>}
     {body && (
       <Stack spacing={[3, 4]}>
         {body && body.length ? body.map((text, key) => <Body key={key}>{text}</Body>) : body}
