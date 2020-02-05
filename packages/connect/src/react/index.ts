aeba57d2b9388e55a8f719ccf4467ce5cd67eb2a
Hank Stoever <hstove@gmail.com>
2020-02-05 10:03:19 -0800

    version bump, export Title

--- packages/connect/src/react/index.ts
+++ packages/connect/src/react/index.ts
@@ -1,3 +1,4 @@
 export { Connect } from './components/connect';
 export { useConnect } from './hooks/useConnect';
 export * from './components/screen';
+export { Title } from './components/typography';
