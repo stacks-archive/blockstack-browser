3c4b9614daf171fa953828f6d8eccff1723ab9cd
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-02-17 15:11:21 -0600

    fix: add ability for screen to be form, and use onSubmit. Additionally added --mode to webpack command

--- packages/connect/src/react/components/screen/screen.tsx
+++ packages/connect/src/react/components/screen/screen.tsx
@@ -6,20 +6,31 @@ import { ScreenLoader } from './screen-loader';
 interface ScreenBodyProps extends FlexProps {
   noMinHeight?: boolean;
   isLoading?: boolean;
+  onSubmit?: () => void;
 }
 
-export const Screen: React.FC<ScreenBodyProps> = ({ noMinHeight, isLoading, children, ...rest }) => (
-  <>
-    <ScreenLoader isLoading={isLoading} />
-    <Flex
-      width="100%"
-      flexDirection="column"
-      minHeight={noMinHeight ? undefined : ['calc(100vh - 57px)', 'unset']}
-      flex={1}
-      style={{ pointerEvents: isLoading ? 'none' : 'unset' }}
-      {...rest}
-    >
-      {children}
-    </Flex>
-  </>
-);
+export const Screen: React.FC<ScreenBodyProps> = ({ noMinHeight, isLoading, children, onSubmit, ...rest }) => {
+  const fn = onSubmit
+    ? (e: React.FormEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        onSubmit();
+      }
+    : undefined;
+  return (
+    <>
+      <ScreenLoader isLoading={isLoading} />
+      <Flex
+        width="100%"
+        flexDirection="column"
+        minHeight={noMinHeight ? undefined : ['calc(100vh - 57px)', 'unset']}
+        flex={1}
+        style={{ pointerEvents: isLoading ? 'none' : 'unset' }}
+        as={onSubmit ? 'form' : 'main'}
+        onSubmit={fn}
+        {...rest}
+      >
+        {children}
+      </Flex>
+    </>
+  );
+};
