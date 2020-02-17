51e1aa18e6220790620931d2c709fa0d4e6ecc38
Hank Stoever <hstove@gmail.com>
2020-02-17 10:19:32 -0800

    refactor: use test subdomain in development

--- packages/app/src/common/constants.ts
+++ packages/app/src/common/constants.ts
@@ -2,4 +2,8 @@ import { Subdomains } from '@blockstack/keychain';
 
 export const gaiaUrl = 'https://hub.blockstack.org';
 
-export const Subdomain = Subdomains.BLOCKSTACK;
+export let Subdomain: Subdomains = Subdomains.BLOCKSTACK;
+
+if (NODE_ENV === 'development') {
+  Subdomain = Subdomains.TEST;
+}
--- packages/app/src/common/dev/types/globals.d.ts
+++ packages/app/src/common/dev/types/globals.d.ts
@@ -1,1 +1,2 @@
 declare const EXT_ENV: string;
+declare const NODE_ENV: string;
