d4c17bc2ae80a63f9b97dd9ef7d461511d1d1a65
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-02-20 10:56:33 -0600

    build: implement better bundling, adjust what is included, use preact, add analyzer

--- packages/connect/webpack.config.js
+++ packages/connect/webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require('webpack');
+
+const analyzeBundle = process.env.ANALYZE === 'true';
 
 module.exports = {
   entry: './src/index.ts',
@@ -33,9 +38,65 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // This is only used in production mode
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            // We want terser to parse ecma 8 code. However, we don't want it
+            // to apply any minification steps that turns valid ecma 5 code
+            // into invalid ecma 5 code. This is why the 'compress' and 'output'
+            // sections only apply transformations that are ecma 5 safe
+            // https://github.com/facebook/create-react-app/pull/4234
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            // Disabled because of an issue with Uglify breaking seemingly valid code:
+            // https://github.com/facebook/create-react-app/issues/2376
+            // Pending further investigation:
+            // https://github.com/mishoo/UglifyJS2/issues/2011
+            comparisons: false,
+            // Disabled because of an issue with Terser breaking valid code:
+            // https://github.com/facebook/create-react-app/issues/5250
+            // Pending further investigation:
+            // https://github.com/terser-js/terser/issues/120
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          // Added for profiling in devtools
+          keep_classnames: false,
+          keep_fnames: false,
+          output: {
+            ecma: 5,
+            comments: false,
+            // Turned on because emoji and regex is not minified properly using default
+            // https://github.com/facebook/create-react-app/issues/2488
+            ascii_only: true,
+          },
+        },
+        sourceMap: false,
+      }),
+    ],
+  },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
+    },
   },
+  devtool: false,
+  plugins: [
+    // BIP39 includes ~240KB of non-english json that we don't currently use.
+    new webpack.IgnorePlugin(/\.\/wordlists\/(?!english\.json)/),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -43,3 +104,7 @@ module.exports = {
     libraryTarget: 'var',
   },
 };
+
+if (analyzeBundle) {
+  module.exports.plugins.push(new BundleAnalyzerPlugin());
+}
