d778c0fcf2fffb781d4ea9b684660c8b362df31b
Hank Stoever <hstove@gmail.com>
2020-03-11 08:23:40 -0700

    fix: prod test-app not linking to prod authenticator

--- packages/test-app/components/app.tsx
+++ packages/test-app/components/app.tsx
@@ -8,10 +8,13 @@ let authOrigin = 'http://localhost:8080';
 // In order to have deploy previews use the same version of the authenticator,
 // we detect if this is a 'deploy preview' and change the origin to point to the
 // same PR's deploy preview in the authenticator.
-if (document.location.origin.includes('deploy-preview')) {
+const { origin } = location;
+if (origin.includes('deploy-preview')) {
   // Our netlify sites are called "authenticator-demo" for this app, and
   // "stacks-authenticator" for the authenticator.
   authOrigin = document.location.origin.replace('authenticator-demo', 'stacks-authenticator');
+} else if (origin.includes('authenticator-demo')) {
+  authOrigin = 'https://app.blockstack.org';
 }
 
 const Card: React.FC = props => (
