86b0841a7de9f06f29ab7f072f19cc2d8a9138a8
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-06-26 16:08:03 -0500

    feat: icons.mdx and icons preview

---
+++ packages/ui-docs/src/components/icons.tsx
@@ -0,0 +1,103 @@
+import {
+  Grid,
+  Flex,
+  Box,
+  AppsIcon,
+  BlockchainIcon,
+  BlockstackIcon,
+  CheckmarkCircleIcon,
+  CheckmarkIcon,
+  ChevronIcon,
+  CloseIcon,
+  ConnectLogoIcon,
+  EncryptionIcon,
+  ExclamationMarkCircleIcon,
+  ExclamationMarkIcon,
+  EyeIcon,
+  PadlockIcon,
+  PlusCircleIcon,
+  PrivateIcon,
+  UnionLineIcon,
+  space,
+} from '@blockstack/ui';
+
+import { InlineCode } from '@components/mdx';
+
+type ComponentNamesLiteral =
+  | `AppsIcon`
+  | `BlockchainIcon`
+  | `BlockstackIcon`
+  | `CheckmarkCircleIcon`
+  | `CheckmarkIcon`
+  | `ChevronIcon`
+  | `CloseIcon`
+  | `ConnectLogoIcon`
+  | `EncryptionIcon`
+  | `ExclamationMarkCircleIcon`
+  | `ExclamationMarkIcon`
+  | `EyeIcon`
+  | `PadlockIcon`
+  | `PlusCircleIcon`
+  | `PrivateIcon`
+  | `UnionLineIcon`;
+
+const strings = [
+  `AppsIcon`,
+  `BlockchainIcon`,
+  `BlockstackIcon`,
+  `CheckmarkCircleIcon`,
+  `CheckmarkIcon`,
+  `ChevronIcon`,
+  `CloseIcon`,
+  `ConnectLogoIcon`,
+  `EncryptionIcon`,
+  `ExclamationMarkCircleIcon`,
+  `ExclamationMarkIcon`,
+  `EyeIcon`,
+  `PadlockIcon`,
+  `PlusCircleIcon`,
+  `PrivateIcon`,
+  `UnionLineIcon`,
+];
+
+const Components = {
+  AppsIcon,
+  BlockchainIcon,
+  BlockstackIcon,
+  CheckmarkCircleIcon,
+  CheckmarkIcon,
+  ChevronIcon,
+  CloseIcon,
+  ConnectLogoIcon,
+  EncryptionIcon,
+  ExclamationMarkCircleIcon,
+  ExclamationMarkIcon,
+  EyeIcon,
+  PadlockIcon,
+  PlusCircleIcon,
+  PrivateIcon,
+  UnionLineIcon,
+};
+
+export const Icons = () => {
+  return (
+    <Grid
+      my={space('extra-loose')}
+      templateColumns={['repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)']}
+      gap={space('extra-loose')}
+    >
+      {strings.map(icon => {
+        const Component = Components[icon];
+
+        return (
+          <Flex flexDirection="column" align="center" justify="center">
+            <Box mb={space('base-loose')}>
+              <Component size={['36px', '36px', '48px']} />
+            </Box>
+            <InlineCode fontSize="10px" style={{ whiteSpace: 'pre' }}>{`<${icon} />`}</InlineCode>
+          </Flex>
+        );
+      })}
+    </Grid>
+  );
+};
