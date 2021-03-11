0c003baaf539dd9d0c53871c2e45c16a0c93de14
Hank Stoever <hstove@gmail.com>
2021-03-11 07:16:51 -0800

    fix: location of zip file after build-ext.sh

--- build-ext.sh
+++ build-ext.sh
@@ -9,7 +9,7 @@ TS=$(date +%Y)$(date +%m)010000
 find -print | while read file; do
     touch -t $TS "$file"
 done
-DEFAULT_DEST="./stacks-wallet-chromium.zip"
+DEFAULT_DEST="../stacks-wallet-chromium.zip"
 DEST=${1:-$DEFAULT_DEST}
 zip -Xro $DEST *
 echo "✅  Extension packaged as $(basename $DEST)"
