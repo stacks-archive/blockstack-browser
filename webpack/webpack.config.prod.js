3efdc552db1e160b46a6154beb07636544d58010
kyranjamie <kyran.burraston@gmail.com>
2021-05-10 14:51:19 +0200

    fix: manifest descrption exceeds 132 chars

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -2,13 +2,14 @@
 const config = require('./webpack.config.base');
 const { ESBuildMinifyPlugin } = require('esbuild-loader');
 
+const shouldMinify = JSON.parse(process.env.MINIFY_PRODUCTION_BUILD || false);
+
 // Basically, disable any code splitting stuff
 config.optimization = {
   ...config.optimization,
   flagIncludedChunks: false,
   concatenateModules: false,
-  // Chrome web store doesn't allow minified code
-  minimize: false,
+  minimize: shouldMinify,
   moduleIds: 'deterministic',
   splitChunks: {
     hidePathInfo: false,
@@ -16,6 +17,15 @@ config.optimization = {
     maxAsyncRequests: Infinity,
     maxInitialRequests: Infinity,
   },
+  ...(shouldMinify
+    ? {
+        minimizer: [
+          new ESBuildMinifyPlugin({
+            target: 'esnext',
+          }),
+        ],
+      }
+    : {}),
 };
 
 config.devtool = false;
