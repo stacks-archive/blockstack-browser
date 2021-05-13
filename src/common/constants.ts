72580c3d46f65197328f004e302a2c82cb5f8c97
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-05-13 10:16:15 -0500

    fix: correctly center window

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -9,3 +9,6 @@ export const STX_DECIMALS = 6;
 export const STACKS_MARKETS_URL = 'https://coinmarketcap.com/currencies/stacks/markets/';
 
 export const KEBAB_REGEX = /[A-Z\u00C0-\u00D6\u00D8-\u00DE]/g;
+
+export const POPUP_WIDTH = 442;
+export const POPUP_HEIGHT = 580;
--- src/common/popup.ts
+++ src/common/popup.ts
@@ -1,3 +1,5 @@
+import { POPUP_HEIGHT, POPUP_WIDTH } from '@common/constants';
+
 interface PopupOptions {
   url?: string;
   title?: string;
@@ -6,49 +8,28 @@ interface PopupOptions {
   skipPopupFallback?: boolean;
 }
 
-// Width 2px wider than in-page dialog.
-// Ensures retina subpixel rounding
-// does not leave slightly blurry underlap
-const defaultWidth = 442;
-const defaultHeight = 580;
+export const popupCenter = ({ url, w = POPUP_WIDTH, h = POPUP_HEIGHT }: PopupOptions) => {
+  chrome.windows.getCurrent(win => {
+    if (!win) throw Error('No chrome window available');
+    const dualScreenLeft = win.left;
+    const dualScreenTop = win.top;
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/open
-export const popupCenter = ({ url, w = defaultWidth, h = defaultHeight }: PopupOptions) => {
-  const win = window;
-  // Safari reports an incorrect browser height
-  const isSafari = (win as any).safari !== undefined;
+    const width = win.width;
+    const height = win.height;
 
-  const browserViewport = {
-    width: win.innerWidth,
-    height: win.outerHeight,
-  };
-  const browserToolbarHeight = win.outerHeight - win.innerHeight;
-  const browserSidepanelWidth = win.outerWidth - win.innerWidth;
+    if (!dualScreenLeft || !dualScreenTop || !width || !height)
+      throw Error('No chrome window available');
 
-  // Such as fixed operating system UI
-  const removeUnusableSpaceX = (coord: number) =>
-    coord - (win.screen.width - win.screen.availWidth);
-  const removeUnusableSpaceY = (coord: number) =>
-    coord - (win.screen.height - win.screen.availHeight);
+    const left = width / 2 - w / 2 + dualScreenLeft;
+    const top = Math.floor(height / 2 - h / 2 + dualScreenTop);
 
-  const browserPosition = {
-    x: removeUnusableSpaceX(win.screenX),
-    y: removeUnusableSpaceY(win.screenY),
-  };
-
-  const left = browserPosition.x + browserSidepanelWidth + (browserViewport.width - w) / 2;
-  const top =
-    browserPosition.y +
-    browserToolbarHeight +
-    (browserViewport.height - h) / 2 +
-    (isSafari ? 48 : 0);
-
-  chrome.windows.create({
-    url,
-    width: w,
-    height: h,
-    top,
-    left,
-    type: 'popup',
+    chrome.windows.create({
+      url,
+      width: w,
+      height: h,
+      top,
+      left,
+      type: 'popup',
+    });
   });
 };
