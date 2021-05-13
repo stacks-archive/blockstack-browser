7778f8efe174a2ba95dc8e2ef66ffebf17053bf7
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-05-13 10:31:20 -0500

    chore: add comments

--- src/common/popup.ts
+++ src/common/popup.ts
@@ -8,19 +8,27 @@ interface PopupOptions {
   skipPopupFallback?: boolean;
 }
 
-export const popupCenter = ({ url, w = POPUP_WIDTH, h = POPUP_HEIGHT }: PopupOptions) => {
+export function popupCenter(options: PopupOptions) {
+  const { url, w = POPUP_WIDTH, h = POPUP_HEIGHT } = options;
+
+  // @see https://developer.chrome.com/docs/extensions/reference/windows/#method-getCurrent
   chrome.windows.getCurrent(win => {
     if (!win) throw Error('No chrome window available');
+
+    // these units take into account the distance from
+    // the farthest left/top sides of all displays
     const dualScreenLeft = win.left;
     const dualScreenTop = win.top;
 
+    // dimensions of the window that originated the action
     const width = win.width;
     const height = win.height;
 
+    // type safety
     if (!dualScreenLeft || !dualScreenTop || !width || !height)
       throw Error('No chrome window available');
 
-    const left = width / 2 - w / 2 + dualScreenLeft;
+    const left = Math.floor(width / 2 - w / 2 + dualScreenLeft);
     const top = Math.floor(height / 2 - h / 2 + dualScreenTop);
 
     chrome.windows.create({
@@ -29,7 +37,8 @@ export const popupCenter = ({ url, w = POPUP_WIDTH, h = POPUP_HEIGHT }: PopupOpt
       height: h,
       top,
       left,
+      focused: true,
       type: 'popup',
     });
   });
-};
+}
