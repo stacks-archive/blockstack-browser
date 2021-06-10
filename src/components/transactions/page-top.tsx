15eab7202f73543d7265857f39b8c6efb69a6af2
kyranjamie <kyran.burraston@gmail.com>
2021-06-10 14:05:39 +0200

    fix: unclear network target, closes #1279

--- src/components/transactions/page-top.tsx
+++ src/components/transactions/page-top.tsx
@@ -4,14 +4,25 @@ import { useOrigin } from '@common/hooks/use-origin';
 import { useTransactionPageTitle } from '@common/hooks/transaction/use-transaction-page-title';
 import { Stack } from '@stacks/ui';
 import { Caption, Title } from '@components/typography';
+import { useCurrentNetwork } from '@common/hooks/use-current-network';
 
 export const TransactionPageTop = memo(() => {
   const transactionRequest = useTransactionRequest();
   const origin = useOrigin();
   const pageTitle = useTransactionPageTitle();
+  const network = useCurrentNetwork();
   if (!transactionRequest) return null;
 
   const appName = transactionRequest?.appDetails?.name;
+
+  const testnetAddition = network.isTestnet ? (
+    <>
+      {' '}
+      on <br />
+      {network.url}
+    </>
+  ) : null;
+
   return (
     <Stack pt="extra-loose" spacing="base">
       <Title fontWeight="bold" as="h1">
@@ -20,6 +31,7 @@ export const TransactionPageTop = memo(() => {
       {appName ? (
         <Caption>
           Requested by {appName} {origin ? `(${origin?.split('//')[1]})` : null}
+          {testnetAddition}
         </Caption>
       ) : null}
     </Stack>
