8602bd8d1acd4a4220c9f55b5893a4ba22e9ef3e
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-07-13 10:22:02 -0500

    fix: refactor name atom for network perf

--- src/store/accounts/names.ts
+++ src/store/accounts/names.ts
@@ -1,26 +1,22 @@
-import { Atom, atom } from 'jotai';
+import { atom } from 'jotai';
 import { accountsWithAddressState } from './index';
 import { currentNetworkState } from '@store/networks';
 import { fetchNamesByAddress } from '@common/api/names';
 import { atomFamilyWithQuery } from '@store/query';
-import { waitForAll } from 'jotai/utils';
+import { makeLocalDataKey } from '@store/common/utils';
 
-function makeKey(networkUrl: string): string {
-  return `${networkUrl}__BNS_NAMES`;
-}
-
-function getLocalNames(networkUrl: string) {
-  const key = makeKey(networkUrl);
+function getLocalNames(networkUrl: string, address: string) {
+  const key = makeLocalDataKey([networkUrl, address, 'BNS_NAMES']);
   const value = localStorage.getItem(key);
   if (!value) return null;
   const [data, date] = JSON.parse(value);
   const now = Date.now();
   if (now - date > STALE_TIME) return null;
-  return data as Record<string, string[]>;
+  return data as string[];
 }
 
-function setLocalNames(networkUrl: string, data: Record<string, string[]>): void {
-  const key = makeKey(networkUrl);
+function setLocalNames(networkUrl: string, address: string, data: string[]): void {
+  const key = makeLocalDataKey([networkUrl, address, 'BNS_NAMES']);
   return localStorage.setItem(key, JSON.stringify([data, Date.now()]));
 }
 
@@ -36,25 +32,25 @@ const STALE_TIME = 30 * 60 * 1000; // 30 min
 
 const namesResponseState = atomFamilyWithQuery<[string, string], string[]>(
   'ACCOUNT_NAMES',
-  (_get, [address, networkUrl]) => {
-    return fetchNamesByAddress(networkUrl, address);
-  }
+  async (_get, [address, networkUrl]) => {
+    let data = getLocalNames(networkUrl, address);
+    if (data) return data;
+    data = await fetchNamesByAddress(networkUrl, address);
+    setLocalNames(networkUrl, address, data);
+    return data;
+  },
+  { keepPreviousData: true }
 );
 
 export const accountNameState = atom<AccountNameState>(get => {
   const accounts = get(accountsWithAddressState);
   const network = get(currentNetworkState);
   if (!network || !accounts) return null;
-  let names = getLocalNames(network.url);
-  if (!names) {
-    let obj: Record<string, Atom<any>> = {};
-    accounts.forEach(account => {
-      obj[account.address] = namesResponseState([account.address, network.url]);
-    });
-    names = get(waitForAll(obj));
-    setLocalNames(network.url, names);
-  }
-  return accounts.map(({ address, index }) => ({ address, index, names: names?.[address] || [] }));
+  return accounts.map(({ address, index }) => ({
+    address,
+    index,
+    names: get(namesResponseState([address, network.url])) || [],
+  }));
 });
 
 accountNameState.debugLabel = 'accountNameState';
