7004aa9f4c52113b12d07aafb18a0601ebe9ff2f
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-07-13 14:49:51 -0500

    fix: try catch

--- src/common/validation/validate-tx-id.ts
+++ src/common/validation/validate-tx-id.ts
@@ -3,5 +3,9 @@ import { with0x } from '@common/utils';
 export function validateTxId(txid: string): boolean {
   const value = with0x(txid).toLowerCase();
   if (value.length !== 66) return false;
-  return with0x(BigInt(value).toString(16).padStart(64, '0')) === value;
+  try {
+    return with0x(BigInt(value).toString(16).padStart(64, '0')) === value;
+  } catch (e) {
+    return false;
+  }
 }
