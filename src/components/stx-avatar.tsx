leather-io/extension

feat: fetch & show microblocks
Fetch microblocks using unanchored=true,  those microblock tx must be
Add dual balances for microblocks
Show icon for microblock in activity
displayed as confirmed
fetches both unanchored & anchored balances
Add new lightning icon to asset icon
Add a new subAmount to Asset components
closes #1428

src/components/stx-avatar.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { StxIcon } from '@components/icons/stx-icon';
+import { MicroblockIcon } from '@components/icons/microblock';
 import { BoxProps, Circle, color, DynamicColorCircle } from '@stacks/ui';
+import { TypeIconWrapper } from '@components/tx-icon';
 
-export const StxAvatar: React.FC<BoxProps> = props => {
+const microIcon = () => (
+  <MicroblockIcon
+    size="13px"
+    fill={color('bg')}
+    borderColor={color('invert')}
+    bg={color('invert')}
+  />
+);
+
+const iconItem = (isUnanchored = false) =>
+  isUnanchored ? <TypeIconWrapper icon={microIcon} bg="invert" /> : null;
+
+export const StxAvatar: React.FC<StxAvatarProps> = props => {
   return (
-    <Circle backgroundColor={color('invert')} {...props}>
+    <Circle position="relative" size="36px" bg={color('invert')} color={color('bg')} {...props}>
       <StxIcon size="12px" />
+      {iconItem(props.isUnanchored)}
     </Circle>
   );
 };
@@ -13,6 +28,11 @@ export const StxAvatar: React.FC<BoxProps> = props => {
 interface AssetProps extends BoxProps {
   gradientString: string;
   useStx: boolean;
+  isUnanchored?: boolean;
+}
+
+interface StxAvatarProps extends BoxProps {
+  isUnanchored?: boolean;
 }
 
 export const AssetAvatar: React.FC<AssetProps> = ({
@@ -27,6 +47,7 @@ export const AssetAvatar: React.FC<AssetProps> = ({
   return (
     <DynamicColorCircle {...props} string={gradientString}>
       {children}
+      {iconItem(props.isUnanchored)}
     </DynamicColorCircle>
   );
 };

stacks-archive/blockstack-browser

src/components/stx-avatar.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { StxIcon } from '@components/icons/stx-icon';
+import { MicroblockIcon } from '@components/icons/microblock';
 import { BoxProps, Circle, color, DynamicColorCircle } from '@stacks/ui';
+import { TypeIconWrapper } from '@components/tx-icon';
 
-export const StxAvatar: React.FC<BoxProps> = props => {
+const microIcon = () => (
+  <MicroblockIcon
+    size="13px"
+    fill={color('bg')}
+    borderColor={color('invert')}
+    bg={color('invert')}
+  />
+);
+
+const iconItem = (isUnanchored = false) =>
+  isUnanchored ? <TypeIconWrapper icon={microIcon} bg="invert" /> : null;
+
+export const StxAvatar: React.FC<StxAvatarProps> = props => {
   return (
-    <Circle backgroundColor={color('accent')} {...props}>
-      <StxIcon />
+    <Circle position="relative" size="36px" bg={color('accent')} color={color('bg')} {...props}>
+      <StxIcon/>
+      {iconItem(props.isUnanchored)}
     </Circle>
   );
 };
@@ -13,6 +28,11 @@ export const StxAvatar: React.FC<BoxProps> = props => {
 interface AssetProps extends BoxProps {
   gradientString: string;
   useStx: boolean;
+  isUnanchored?: boolean;
+}
+
+interface StxAvatarProps extends BoxProps {
+  isUnanchored?: boolean;
 }
 
 export const AssetAvatar: React.FC<AssetProps> = ({
@@ -27,6 +47,7 @@ export const AssetAvatar: React.FC<AssetProps> = ({
   return (
     <DynamicColorCircle {...props} string={gradientString}>
       {children}
+      {iconItem(props.isUnanchored)}
     </DynamicColorCircle>
   );
 };